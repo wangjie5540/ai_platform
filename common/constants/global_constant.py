112c13ffc39bd5437d555e3492cb243e5869b68d
王同贵 <wangtonggui@sensorsdata.cn>
2022-05-12 11:24:59 +0800

    添加算法平台统一的pvc

--- common/constants/global_constant.py
+++ common/constants/global_constant.py
@@ -4,3 +4,5 @@ component_output_path = '/tmp/component_output'
 mount_nfs_dir = '/mnt/nfs'
 image_registry = 'digit-force-docker.pkg.coding.net/marketing_algorithm'
 registry_db = 'hello-world'
+# 默认的pvc，需要在部署完毕之后同步创建
+default_pvc = 'ai-platform-pvc'
