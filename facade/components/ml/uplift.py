70bf7fbed2789dd93a3bb9a632e3ec3581d3862e
王同贵 <wangtonggui@sensorsdata.cn>
2022-05-13 14:57:31 +0800

    1. 添加创建配置与secret的yaml 2. 优化uplift门面

--- facade/components/ml/uplift.py
+++ facade/components/ml/uplift.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from kfp.dsl import ContainerOp
+from kfp.dsl import ContainerOp, PipelineVolume
 
 import common.constants.global_constant as global_constant
 from common.utils import component_helper
@@ -14,14 +14,14 @@ out_1 = component_helper.get_output(component_name)
 
 # TODO 后续做成门面模式，单独提供pip包
 class UpliftOp(ContainerOp):
-    def __init__(self, input_file, volume):
+    def __init__(self, input_1, image=image_full_name):
         super(UpliftOp, self).__init__(
             name=component_name,
-            image=image_full_name,
+            image=image,
             command=['python', 'uplift_model.py'],
-            arguments=['--input_file', input_file],
+            arguments=['--input_1', input_1],
             file_outputs={
                 'out_1': out_1
             },
-            pvolumes={'/mnt/nfs': volume}
+            pvolumes={global_constant.mount_nfs_dir: PipelineVolume(pvc=global_constant.default_pvc)}
         )
