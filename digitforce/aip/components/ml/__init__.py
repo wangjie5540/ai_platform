1f27b5f133d838785b54d5fe62fb6472ba2db976
wh <cuebwanghan@163.com>
2022-05-24 15:45:39 +0800

    update

---
+++ digitforce/aip/components/ml/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+'''
+@file: __init__.py.py
+@time: 2022/5/24 14:53
+@desc:
+'''
---
+++ digitforce/aip/components/ml/lookalike.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+'''
+@file: lookalike.py
+@time: 2022/5/24 14:53
+@desc:
+'''
