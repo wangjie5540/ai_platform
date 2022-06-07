5ca7f4b1c868fe535b8c6999d929aadacb3d18f4
wanghansen <wanghansen@meituan.com>
2022-06-07 10:46:37 +0800

    新增shapley苏建

--- src/causal/shapley/main.py
+++ src/causal/shapley/main.py
@@ -1,15 +1,15 @@
-
 import argparse
-from model import markov
+from model import shapley
 from digitforce.aip.common.logging_config import setup_console_log
 import logging
+
 
 def run():
     setup_console_log()
     # 解析输入参数
     parser = argparse.ArgumentParser()
 
-    # data.csv样例：
+    # 输入data.csv样例：
     # path;total_conversions
     # Email;110
     # Email > Facebook;11
@@ -20,13 +20,18 @@ def run():
     parser.add_argument('--input_file', type=str, default="data.csv", help='input filename')
     parser.add_argument('--output_file', type=str, default="output.txt", help='output filename')
 
+    # 模型1: simply，基于传统shapley的简化，原文：https://arxiv.org/abs/1804.05327，实现参考：https://github.com/ianchute/shapley-attribution-model-zhao-naive
+    # 模型2: ordered，在simply基础上计算每个渠道在各个触点上的贡献度，从1维扩展到n维，实现参考同上
+    parser.add_argument('--model_type', type=str, default="simply", help='simply or ordered')
+
     args = parser.parse_args()
     logging.info(f"参数解析完毕. args={args}")
-    markov(args.input_file, args.output_file)
+    shapley(args.input_file, args.output_file, args.model_type)
 
     # 向下游传递参数
     # TODO 后续将进行封装
     # component_helper.pass_output({'out_1': out_1})
+
 
 if __name__ == '__main__':
     run()
---
+++ src/causal/shapley/simplified_shapley_attribution_model.py
@@ -0,0 +1,31 @@
+from itertools import chain, combinations
+from tqdm import tqdm
+from collections import Counter
+
+
+class SimplifiedShapleyAttributionModel:
+    def powerset(self, x):
+        s = list(x)
+        return chain.from_iterable(combinations(s, r) for r in range(len(s) + 1))
+
+    def _phi(self, channel_index):
+        S_channel = [k for k in self.journeys.keys() if channel_index in k]
+        score = 0
+        # print(f"Computing phi for channel {channel_index}...")
+        for S in tqdm(S_channel):
+            score += self.journeys[S] / len(S)
+        # print(f"Attribution score for channel {channel_index}: {score:.2f}")
+        # print()
+        return score
+
+    def attribute(self, journeys):
+        self.P = set(chain(*journeys))
+        print("Running Simplified Shapley Attribution Model...")
+        print(f"Found {len(self.P)} unique channels!")
+        
+        # print("Computing journey statistics...")
+        self.journeys = Counter([frozenset(journey) for journey in journeys])
+        
+        # print(f"Computing attributions...")
+        # print()
+        return {j: self._phi(j) for j in self.P}
