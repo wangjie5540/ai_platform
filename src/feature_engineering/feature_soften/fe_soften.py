671ec29d6a1cce837fd29d7b05ec3839c8ff70bd
wufei <wufei@digitforce.com>
2022-06-07 17:00:16 +0800

    特征异常平滑

--- src/feature_engineering/feature_soften/fe_soften.py
+++ src/feature_engineering/feature_soften/fe_soften.py
@@ -5,8 +5,8 @@ def soften(df, soften_method='z-score', cols=None, thresh_max=None, thresh_min=N
     """
     特征异常平滑
     @param df: 输入数据
-    @param soften_method: 平滑方式(z-score, thresh, percent)
-    @param cols: 平滑列名
+    @param soften_method: 平滑方式(z-score, thresh, percent, box_plot)
+    @param cols: list, 平滑列名
     @param thresh_max: 阈值上限，当平滑方式为阈值时，需配置改参数
     @param thresh_min: 阈值下限，当平滑方式为阈值时，需配置改参数
     @param percent_max: 百分数上限， 当平滑方式为百分位时，需配置改参数
@@ -17,9 +17,13 @@ def soften(df, soften_method='z-score', cols=None, thresh_max=None, thresh_min=N
         df[cols] = df[cols].apply(z_score_soften, axis=0)
     elif soften_method == 'thresh':
         df[cols].clip(thresh_min, thresh_max, inplace=True)
+    elif soften_method == 'box_plot':
+        df[cols] = df[cols].apply(boxplot_soften, axis=0)
     elif soften_method == 'percent':
-        df[cols] = df[cols].apply(percent_soften, axis=0)
-        return df
+        df[cols] = df[cols].apply(percent_soften, args=(percent_max, percent_min), axis=0)
+    else:
+        raise ValueError('请选择平滑方式')
+    return df
 
 
 def z_score_soften(series):
@@ -30,8 +34,14 @@ def z_score_soften(series):
     return series.clip(min_value, max_value)
 
 
-def percent_soften(series):
+def boxplot_soften(series):
     q1 = series.quantile(0.25)
     q3 = series.quantile(0.75)
     iqr = q3 - q1
     return series.clip(q1 - 1.5 * iqr, q3 + 1.5 * iqr)
+
+
+def percent_soften(series, percent_max, percent_min):
+    min_value = series.quantile(percent_min)
+    max_value = series.quantile(percent_max)
+    return series.clip(min_value, max_value)
--- src/feature_engineering/feature_soften/main.py
+++ src/feature_engineering/feature_soften/main.py
@@ -1,2 +1,26 @@
 from fe_soften import soften
 
+
+def main():
+
+    import sys
+    df = sys.argv[1]
+    soften_method = sys.argv[2]
+    cols = sys.argv[3].split(',')
+    thresh_max = thresh_min = percent_max = percent_min = None
+    if soften_method == 'thresh':
+        if not sys.argv[4] or not sys.argv[5]:
+            raise ValueError('thresh_max and thresh_min must be required')
+        thresh_max = sys.argv[4]
+        thresh_min = sys.argv[5]
+
+    elif soften_method == 'percent':
+        if not sys.argv[4] or not sys.argv[5]:
+            raise ValueError('percent_max and percent_min must be required')
+        percent_max = sys.argv[4]
+        percent_min = sys.argv[5]
+    soften(df, soften_method, cols, thresh_max, thresh_min, percent_max, percent_min)
+
+
+if __name__ == '__main__':
+    main()
