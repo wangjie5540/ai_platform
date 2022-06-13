4117932ee773f39aa77346972266687ab6badb28
wufei <wufei@digitforce.com>
2022-06-13 16:16:44 +0800

    pipeline

--- digitforce/aip/components/feature_engineering/soften.py
+++ digitforce/aip/components/feature_engineering/soften.py
@@ -3,23 +3,24 @@ from digitforce.aip.components.op_decorator import *
 
 
 @mount_data_pv
-def fe_soften_op(input_file, sep=',', soften_method='z-score', cols=None, thresh_max=None, thresh_min=None,
+def fe_soften_op(input_file, output_file,  sep=',', soften_method='z-score', cols=None, thresh_max=None, thresh_min=None,
                  percent_max=None, percent_min=None, image_tag="latest"):
     """
 
-    @param image_tag:
-    @param input_file:
-    @param sep:
-    @param soften_method:
-    @param cols:
-    @param thresh_max:
-    @param thresh_min:
-    @param percent_max:
-    @param percent_min:
+    @param input_file: 输入数据
+    @param output_file: 输出文件路径
+    @param sep: 分隔符
+    @param soften_method: 平滑方式(z-score, thresh, percent, box_plot)
+    @param cols: list, 平滑列名
+    @param thresh_max: 阈值上限，当平滑方式为阈值时，需配置改参数
+    @param thresh_min: 阈值下限，当平滑方式为阈值时，需配置改参数
+    @param percent_max: 百分数上限， 当平滑方式为百分位时，需配置改参数
+    @param percent_min: 百分数下限， 当平滑方式为百分位时，需配置改参数
+    @param image_tag: 版本
     """
     return dsl.ContainerOp(name="feature_soften",
                            image=f"{AI_PLATFORM_IMAGE_REPO}"
                                  f"/src-feature_engineering-feature_soften" + f":{image_tag}",
                            command="python",
-                           arguments=['main.py', input_file, sep, soften_method, cols, thresh_max,
+                           arguments=['main.py', input_file, output_file, sep, soften_method, cols, thresh_max,
                                       thresh_min, percent_max, percent_min])
