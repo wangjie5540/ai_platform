2b43a90f5714a67fc06217dce91a331f4af174d4
汤思璇 <tangsixuan@digitforce.com>
2022-06-14 15:09:22 +0800

    复购训练组件更改读取方式为Spark+Starrocks

--- src/ml/repurchase/spark_env.py
+++ src/ml/repurchase/spark_env.py
@@ -1,31 +1,24 @@
-# import findspark
-# findspark.init()
-# import os
-# from pyspark.sql import SparkSession
-# import pandas
-# from pyspark.sql.types import *
-# from pyspark.sql.functions import *
-# # from graphframes import *
-# import pandas as pd
-# import numpy as np
-# import time
-
-
-# coding: utf-8
-import subprocess
-import findspark
 import os
+
 os.environ['SPARK_HOME'] = '/bd-components/cloudera/parcels/CDH-6.3.1-1.cdh6.3.1.p0.1470567/lib/spark'
 os.environ['JAVA_HOME'] = '/usr/java/jdk1.8.0_181-cloudera'
-os.environ['PYSPARK_PYTHON'] = "./pjy-pyspark3.6/pjy-pyspark3.6/bin/python"
-findspark.init()
+os.environ['PYSPARK_PYTHON'] = "./pjy-pyspark3.6.2.zip/pjy-pyspark3.6/bin/python"
+import findspark
 
+findspark.init()
 from pyspark.sql import SparkSession
 
 class SparkEnv:
     def __new__(self, envname):
-        self.spark = SparkSession.builder.master('yarn')\
-            .appName(envname)\
-            .config("spark.yarn.dist.archives", "/data/anaconda3/envs/pjy-pyspark3.6.zip#pjy-pyspark3.6")\
-            .enableHiveSupport().getOrCreate()
+        self.spark = SparkSession.builder.master('yarn') \
+                    .appName(envname) \
+                    .config("spark.yarn.dist.archives", "hdfs:///tmp/wtg/pjy-pyspark3.6.2.zip") \
+                    .config("spark.jars", "hdfs:///user/algorithm/starrocks-spark2_2.11-1.0.0.jar") \
+                    .enableHiveSupport().getOrCreate()
         return self.spark
+
+def spark_read(spark, table_name, tempViewName):
+    df = spark.read.format('starrocks').option('starrocks.table.identifier', f'{table_name}').option(
+        'starrocks.fenodes', 'bigdata-server-07:8030').option('user', 'root').option('password', '').load()
+    df.createOrReplaceTempView(f'{tempViewName}')
+    print(f'success creating temp view: {tempViewName}')
