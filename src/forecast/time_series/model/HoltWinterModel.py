e391e2edd228641983f0f3504547175bef72ea19
wangfeng <wangfeng@digitforce.com>
2022-06-22 19:16:35 +0800

    holtWinter update

--- src/forecast/time_series/model/HoltWinterModel.py
+++ src/forecast/time_series/model/HoltWinterModel.py
@@ -15,15 +15,6 @@ class HoltWinterModel():
         self.data=data
         self.param=param
 
-        seasonal_periods=self.param['seasonal_periods'] if self.param['seasonal_periods'] is not None else None
-        trend=self.param['trend'] if self.param['trend'] is not None else None
-        seasonal=self.param['seasonal'] if self.param['seasonal'] is not None else None
-        damped_trend=self.param['damped_trend'] if self.param['damped_trend'] is not None else None
-        use_boxcox=self.param['use_boxcox'] if self.param['use_boxcox'] is not None else None
-        initialization_method=self.param['initialization_method'] if self.param['initialization_method'] is not None else None
-        missing=self.param['missing'] if self.param['missing'] is not None else None
-        freq=self.param['freq'] if self.param['freq'] is not None else None
-
         if self.param['seasonal_periods'] is not None:
             if self.param['trend'] is not None:
                 if self.param['seasonal'] is not None:
@@ -34,70 +25,70 @@ class HoltWinterModel():
                                     if self.param['freq'] is not None:
                                         self.model = ExponentialSmoothing(
                                             self.data,
-                                            seasonal_periods=seasonal_periods,
-                                            trend=trend,
-                                            seasonal=seasonal,
-                                            damped_trend=damped_trend,
-                                            use_boxcox=use_boxcox,
-                                            initialization_method=initialization_method,
-                                            missing=missing,
-                                            freq=freq
+                                            seasonal_periods=self.param['seasonal_periods'],
+                                            trend=self.param['trend'],
+                                            seasonal=self.param['seasonal'],
+                                            damped_trend=self.param['damped_trend'],
+                                            use_boxcox=self.param['use_boxcox'],
+                                            initialization_method=self.param['initialization_method'],
+                                            missing=self.param['missing'],
+                                            freq=self.param['freq']
                                         )
                                     else:
                                         self.model = ExponentialSmoothing(
                                             self.data,
-                                            seasonal_periods=seasonal_periods,
-                                            trend=trend,
-                                            seasonal=seasonal,
-                                            damped_trend=damped_trend,
-                                            use_boxcox=use_boxcox,
-                                            initialization_method=initialization_method,
-                                            missing=missing
+                                            seasonal_periods=self.param['seasonal_periods'],
+                                            trend=self.param['trend'],
+                                            seasonal=self.param['seasonal'],
+                                            damped_trend=self.param['damped_trend'],
+                                            use_boxcox=self.param['use_boxcox'],
+                                            initialization_method=self.param['initialization_method'],
+                                            missing=self.param['missing']
                                         )
                                 else:
                                     self.model = ExponentialSmoothing(
                                         self.data,
-                                        seasonal_periods=seasonal_periods,
-                                        trend=trend,
-                                        seasonal=seasonal,
-                                        damped_trend=damped_trend,
-                                        use_boxcox=use_boxcox,
-                                        initialization_method=initialization_method
+                                        seasonal_periods=self.param['seasonal_periods'],
+                                        trend=self.param['trend'],
+                                        seasonal=self.param['seasonal'],
+                                        damped_trend=self.param['damped_trend'],
+                                        use_boxcox=self.param['use_boxcox'],
+                                        initialization_method=self.param['initialization_method']
                                     )
                             else:
                                 self.model = ExponentialSmoothing(
                                     self.data,
-                                    seasonal_periods=seasonal_periods,
-                                    trend=trend,
-                                    seasonal=seasonal,
-                                    damped_trend=damped_trend,
-                                    use_boxcox=use_boxcox
+                                    seasonal_periods=self.param['seasonal_periods'],
+                                    trend=self.param['trend'],
+                                    seasonal=self.param['seasonal'],
+                                    damped_trend=self.param['damped_trend'],
+                                    use_boxcox=self.param['use_boxcox']
                                 )
                         else:
                             self.model = ExponentialSmoothing(
                                 self.data,
-                                seasonal_periods=seasonal_periods,
-                                trend=trend,
-                                seasonal=seasonal,
-                                damped_trend=damped_trend
+                                seasonal_periods=self.param['seasonal_periods'],
+                                trend=self.param['trend'],
+                                seasonal=self.param['seasonal'],
+                                damped_trend=self.param['damped_trend']
                             )
                     else:
                         self.model = ExponentialSmoothing(
                             self.data,
-                            seasonal_periods=seasonal_periods,
-                            trend=trend,
-                            seasonal=seasonal
+                            seasonal_periods=self.param['seasonal_periods'],
+                            trend=self.param['trend'],
+                            seasonal=self.param['seasonal']
                         )
                 else:
                     self.model = ExponentialSmoothing(
                         self.data,
-                        seasonal_periods=seasonal_periods,
-                        trend=trend
+                        seasonal_periods=self.param['seasonal_periods'],
+                        trend=self.param['trend']
                     )
             else:
                 self.model = ExponentialSmoothing(
                     self.data,
-                    seasonal_periods=seasonal_periods
+                    seasonal_periods=self.param['seasonal_periods']
                 )
         else:
             self.model = ExponentialSmoothing(
