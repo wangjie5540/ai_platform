9f0040341e01b659575028ce5ac849c671e94fb8
wangfeng <wangfeng@digitforce.com>
2022-06-23 16:03:15 +0800

    update HoltWinterModel

--- src/forecast/time_series/model/HoltWinterModel.py
+++ src/forecast/time_series/model/HoltWinterModel.py
@@ -10,41 +10,37 @@ from statsmodels.tsa.api import ExponentialSmoothing
 
 class HoltWinterModel():
 
-    def __init__(self,data,param=None):
+    def __init__(self,data,param=None,param_fit=None):
 
         self.data=data
         self.param=param
+        self.param_fit = param_fit
 
-        trend = None
-        damped_trend = False
-        seasonal = None
-        seasonal_periods = None
-        initialization_method = "estimated"
-        inittial_level = None
-        inittial_trend = None
-        initial_seasonal = None
-        use_boxcox = False
-        bounds = None
-        freq = None
-        missing = "none"
-        dates = None
+        default_param = {
+            "trend": None,
+            "damped_trend": False,
+            "seasonal": None,
+            "seasonal_periods": None,
+            "initialization_method": "estimated",
+            "inittial_level": None,
+            "inittial_trend": None,
+            "initial_seasonal": None,
+            "use_boxcox": False,
+            "bounds": None,
+            "freq": None,
+            "missing": "none",
+            "dates": None
+        }.update(self.param)
 
-        if self.param['seasonal_periods'] is not None:
-            seasonal_periods = self.param['seasonal_periods']
-        if self.param['trend'] is not None:
-            trend = self.param['trend']
-        if self.param['seasonal'] is not None:
-            seasonal = self.param['seasonal']
-        if self.param['damped_trend'] is not None:
-            damped_trend = self.param['damped_trend']
-        if self.param['use_boxcox'] is not None:
-            use_boxcox = self.param['use_boxcox']
-        if self.param['initialization_method'] is not None:
-            initialization_method = self.param['initialization_method']
-        if self.param['missing'] is not None:
-            missing = self.param['missing']
-        if self.param['freq'] is not None:
-            freq = self.param['freq']
+        seasonal_periods = default_param['seasonal_periods']
+        trend = default_param["trend"]
+        seasonal = default_param["seasonal"]
+        damped_trend = default_param["damped_trend"]
+        use_boxcox = default_param["use_boxcox"]
+        initialization_method = default_param["initialization_method"]
+        missing = default_param["missing"]
+        freq = default_param["freq"]
+
 
         self.model = ExponentialSmoothing(
             self.data,
@@ -60,52 +56,45 @@ class HoltWinterModel():
 
 
     def fit(self):
-        smoothing_level = None
-        smoothing_trend = None
-        smoothing_seasonal = None
-        damping_trend = None
-        optimized = True
-        remove_bias = False
-        start_params = None
-        method = None
-        minimize_kwargs = None
-        use_brute = True
-        use_boxcox = None
-        use_basinhopping = None
-        initial_level = None
-        initial_trend = None
-        if self.param['smoothing_level'] is not None:
-            smoothing_level = self.param['smoothing_level']
-        if self.param['smoothing_trend'] is not None:
-            smoothing_trend = self.param['smoothing_trend']
-        if self.param['smoothing_seasonal'] is not None:
-            smoothing_seasonal = self.param['smoothing_seasonal']
-        if self.param['damping_trend'] is not None:
-            damping_trend = self.param['damping_trend']
-        if self.param['optimized'] is not None:
-            optimized = self.param['optimized']
-        if self.param['remove_bias'] is not None:
-            remove_bias = self.param['remove_bias']
-        if self.param['start_params'] is not None:
-            start_params = self.param['start_params']
-        if self.param['method'] is not None:
-            method = self.param['method']
-        if self.param['minimize_kwargs'] is not None:
-            minimize_kwargs = self.param['minimize_kwargs']
-        if self.param['use_brute'] is not None:
-            use_brute = self.param['use_brute']
-        if self.param['use_boxcox'] is not None:
-            use_boxcox = self.param['use_boxcox']
-        if self.param['use_basinhopping'] is not None:
-            use_basinhopping = self.param['use_basinhopping']
-        if self.param['initial_level'] is not None:
-            initial_level = self.param['initial_level']
-        if self.param['initial_trend'] is not None:
-            initial_trend = self.param['initial_trend']
 
-        self.model=self.model.fit(smoothing_level,smoothing_trend,smoothing_seasonal,damping_trend,optimized,remove_bias,start_params,method,minimize_kwargs,use_brute,use_boxcox,use_basinhopping,initial_level,initial_trend)
+        default_fit_param = {
+            "smoothing_level": None,
+            "smoothing_trend": None,
+            "smoothing_seasonal":None,
+            "damping_trend": None,
+            "optimized":True,
+            "remove_bias": False,
+            "start_params": None,
+            "method": None,
+            "minimize_kwargs": None,
+            "use_brute": True,
+            "use_boxcox": None,
+            "use_basinhopping": None,
+            "initial_level": None,
+            "initial_trend":None
+        }.update(self.param_fit)
+
+        smoothing_level = default_fit_param["smoothing_level"]
+        smoothing_trend = default_fit_param["smoothing_trend"]
+        smoothing_seasonal = default_fit_param["smoothing_seasonal"]
+        damping_trend = default_fit_param["damping_trend"]
+        optimized = default_fit_param["optimized"]
+        remove_bias = default_fit_param["remove_bias"]
+        start_params = default_fit_param["start_params"]
+        method = default_fit_param["method"]
+        minimize_kwargs = default_fit_param["minimize_kwargs"]
+        use_brute = default_fit_param["use_brute"]
+        use_boxcox = default_fit_param["use_boxcox"]
+        use_basinhopping = default_fit_param["use_basinhopping"]
+        initial_level = default_fit_param["initial_level"]
+        initial_trend = default_fit_param["initial_trend"]
+
+        self.model=self.model.fit(smoothing_level,smoothing_trend,smoothing_seasonal,damping_trend,optimized,remove_bias,
+                                  start_params,method,minimize_kwargs,use_brute,use_boxcox,
+                                  use_basinhopping,initial_level,initial_trend)
         return self.model
         
     def forcast(self,predict_len):
         preds=self.model.forecast(predict_len)
         return preds
+
