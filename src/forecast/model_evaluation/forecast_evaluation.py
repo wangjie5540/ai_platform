0b75855cdcda105261cebcc1db894028732a5d5f
anweiguo <anweiguo@digitforce.com>
2022-07-11 16:05:40 +0800

    计算wmape指标

--- src/forecast/model_evaluation/forecast_evaluation.py
+++ src/forecast/model_evaluation/forecast_evaluation.py
@@ -18,12 +18,14 @@ def forecast_evaluation_wmape(df, col_true, col_pred, col_key=[], df_type='pd'):
 
     else:
         df['error'] = abs(df[col_true] - df[col_pred])
+        print("error",df)
         if len(col_key) == 0:
-            df['tmp'] == 'total'
-            agg_df = df.groupby(['tmp']).agg({col_true: sum, col_pred: sum})
+            df['total'] = 'total'
+            agg_df = df.groupby(['total']).agg({col_true: sum, "error": sum})
         else:
-            agg_df = df.groupby(col_key).agg({col_true: sum, col_pred: sum})
+            agg_df = df.groupby(col_key).agg({col_true: sum, "error": sum})
         agg_df[col_true] = agg_df[col_true].apply(lambda x: 1 if x == 0 else x)
+        print(agg_df)
         agg_df['wmape'] = agg_df['error']/agg_df[col_true]
         agg_df = agg_df.reset_index()
         return agg_df
