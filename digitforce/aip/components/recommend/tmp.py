7a3277fa0d26267bba9454012ca5d7afe0b827b4
zhangxueren <zhangxueren@digitforce.com>
2022-07-15 17:42:14 +0800

    update recommend pipeline

---
+++ digitforce/aip/components/recommend/tmp.py
@@ -0,0 +1,28 @@
+from digitforce.aip.common.constants.global_constant import AI_PLATFORM_IMAGE_REPO
+from digitforce.aip.components.op_decorator import *
+
+
+@mount_data_pv
+def rank_data_process_op(sql, output_file, info_log_file, error_log_file, image_tag='latest'):
+    op = dsl.ContainerOp(
+        name='rank_data_process_op',
+        image=f"{AI_PLATFORM_IMAGE_REPO}"
+              f"/pipelines-recommend_rank-data_process" + f":{image_tag}",
+        command="python",
+        arguments=["main.py", sql, output_file, info_log_file, error_log_file]
+    )
+    op.container.set_image_pull_policy('Always')
+    return op
+
+
+@mount_data_pv
+def lightgbm_train_op(dataset_path, model_output_file, info_log_file, error_log_file, image_tag='latest'):
+    op = dsl.ContainerOp(
+        name='lightgbm_train_op',
+        image=f"{AI_PLATFORM_IMAGE_REPO}"
+              f"/pipelines-recommend_rank-lightgbm" + f":{image_tag}",
+        command="python",
+        arguments=["main.py", dataset_path, model_output_file, info_log_file, error_log_file]
+    )
+    op.container.set_image_pull_policy('Always')
+    return op
