6dfe7809c3ad0c85c6c452b066fc05c50d92cc78
汤思璇 <tangsixuan@digitforce.com>
2022-07-18 18:55:01 +0800

    高潜开发

--- src/ml/repurchase/spark_env.py
+++ src/ml/repurchase/spark_env.py
@@ -1,20 +1,10 @@
 import os
 
-os.environ['SPARK_HOME'] = '/bd-components/cloudera/parcels/CDH-6.3.1-1.cdh6.3.1.p0.1470567/lib/spark'
-os.environ['JAVA_HOME'] = '/usr/java/jdk1.8.0_181-cloudera'
-os.environ['PYSPARK_PYTHON'] = "./pjy-pyspark3.6.2.zip/pjy-pyspark3.6/bin/python"
-import findspark
-
-findspark.init()
-from pyspark.sql import SparkSession
+from digitforce.aip.common.utils.spark_helper import SparkClient
 
 class SparkEnv:
     def __new__(self, envname):
-        self.spark = SparkSession.builder.master('yarn') \
-                    .appName(envname) \
-                    .config("spark.yarn.dist.archives", "hdfs:///tmp/wtg/pjy-pyspark3.6.2.zip") \
-                    .config("spark.jars", "hdfs:///user/algorithm/starrocks-spark2_2.11-1.0.0.jar") \
-                    .enableHiveSupport().getOrCreate()
+        self.spark = SparkClient().get_session()
         return self.spark
 
 def spark_read(spark, table_name, tempViewName):
