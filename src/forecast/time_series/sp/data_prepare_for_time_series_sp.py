5f39931781f101d1158c010d4fddfe3edf86bbf4
wangfeng <wangfeng@digitforce.com>
2022-07-18 22:04:32 +0800

    update

--- src/forecast/time_series/sp/data_prepare_for_time_series_sp.py
+++ src/forecast/time_series/sp/data_prepare_for_time_series_sp.py
@@ -10,53 +10,12 @@ from datetime import datetime
 
 import pandas as pd
 from forecast.common.log import get_logger
-from forecast.common.date_helper import date_add_str
-from pyspark.sql.functions import max
+from forecast.common.date_helper import date_add_str,
+# from pyspark.sql.functions import max,date_format,to_date,column
+from pyspark.sql.functions as psf
 from forecast.common.common_helper import *
+from forecast.common.data_helper import row_transform_to_dataFrame
 
-# #后续优化：check对周和月的支持 PS：在param的设置
-# def data_prepare(spark,param):
-#     """
-#     样本选择
-#     :param spark:spark
-#     :param param: 选择样本参数
-#     :return: 样本
-#     """
-#     logger_info=get_logger()
-#     table_sku_grouping=param['table_sku_group']
-#     ts_model_list=param['ts_model_list']
-#     table_feat_y=param['feat_y']
-#     y_type_list=param['y_type_list']
-#     cols_sku_grouping=param['cols_sku_grouping']
-#     apply_model=param['apply_model']
-#     y_type=param['y_type']
-#     cols_feat_y=param['cols_feat_y']
-#     sample_join_key=param['sample_join_key']
-#     dt=param['dt']
-#     edate=param['edate']
-#     sdate=param['sdate']
-#
-#     try:
-#         #sku分类分组表
-#         data_sku_grouping=spark.table(table_sku_grouping).select(cols_sku_grouping)
-#         data_sku_grouping=data_sku_grouping.filter(data_sku_grouping[apply_model].isin(ts_model_list))
-#
-#         #y值表
-#         data_feat_y=spark.table(table_feat_y).select(cols_feat_y)
-#         data_feat_y=data_feat_y.filter(data_feat_y[y_type].isin(y_type_list))
-#         if edate == '' or sdate == '':
-#             edate=data_feat_y.select([max(dt)]).head(1)[0][0]#获取最大值
-#             sdate=date_add_str(edate,-365)#默认一年
-#         data_feat_y = data_feat_y.filter((data_feat_y[dt] >= sdate) & (data_feat_y[dt]<= edate))
-#         data_result=data_feat_y.join(data_sku_grouping, on=sample_join_key, how='inner')
-#
-#         logger_info.info("sample_select_sp 成功")
-#     except Exception as e:
-#         data_result=None
-#         logger_info.info(traceback.format_exc())
-#     return data_result
-
-#检验数据的连续性，如果不连续则补齐
 def data_precess(param,spark):
     table_sku_grouping = param['table_sku_group']
     ts_model_list = param['ts_model_list']
@@ -80,12 +39,12 @@ def data_precess(param,spark):
         #y值表
         data_feat_y=spark.table(table_feat_y).select(cols_feat_y)
         if edate == '' or sdate == '':
-            edate=data_feat_y.select([max(dt)]).head(1)[0][0]#获取最大值
+            edate=data_feat_y.select([psf.max(dt)]).head(1)[0][0]#获取最大值
             sdate=date_add_str(edate,-365)#默认一年
         data_feat_y = data_feat_y.filter((data_feat_y[dt] >= sdate) & (data_feat_y[dt]<= edate))
         data_result=data_feat_y.join(data_sku_grouping, on=sample_join_key, how='inner')
 
-        df = data_result.toPandas()
+        df = row_transform_to_dataFrame(data_result)
 
         df[dt] = df[dt].apply(lambda x: pd.to_datetime(x))
         ts = pd.DataFrame(pd.date_range(start=df.dt.min(), end=df.dt.max()), columns=[dt])
@@ -117,7 +76,7 @@ def data_precess(param,spark):
 
         sales_cleaned = sales_cleaned[['shop_id', 'goods_id', 'th_y', 'apply_model', 'dt']].sort_values('dt')
         sales_cleaned = sales_cleaned.dropna()
-        sales_cleaned["dt"] = sales_cleaned["dt"].apply(lambda x: datetime.datetime.strftime(x, "%Y-%m-%d"))
+        sales_cleaned["dt"] = sales_cleaned["dt"].apply(lambda x: datetime.datetime.strftime(x, "%Y%m%d"))
 
         df_value = sales_cleaned.values.tolist()
         df_columns = sales_cleaned.columns
