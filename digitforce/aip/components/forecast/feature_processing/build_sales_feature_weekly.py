f1f802c92f116596f0d9dcf2b8b3cef8cb279798
gaoxiancheng <gaoxiancheng@digitforce.com>
2022-08-03 17:41:30 +0800

    修改组件中command的格式

--- digitforce/aip/components/forecast/feature_processing/build_sales_feature_weekly.py
+++ digitforce/aip/components/forecast/feature_processing/build_sales_feature_weekly.py
@@ -26,8 +26,8 @@ def build_sales_feature_weekly(sdate, edate, col_time, col_qty, image_tag="lates
     return dsl.ContainerOp(name="build_sales_feature_weekly",
                            image=f"{AI_PLATFORM_IMAGE_REPO}"
                                  f"/src-forecast-image" + f":{image_tag}",
-                           command='/data/entrypoint.sh',
-                           arguments=['spark-submit', '--master', 'yarn', '--deploy-mode', 'cluster', '--conf',
+                           command='bash',
+                           arguments=['/data/entrypoint.sh','spark-submit', '--master', 'yarn', '--deploy-mode', 'cluster', '--conf',
                                       'spark.yarn.appMasterEnv.PYSPARK_PYTHON=./environment/ibs/bin/python',
                                       '--conf', 'spark.yarn.dist.archives=hdfs:///user/awg/ibs.zip#environment',
                                       '--driver-memory', '8G', '--py-files', './forecast.zip,digitforce.zip',
