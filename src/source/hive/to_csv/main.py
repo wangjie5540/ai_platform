0ea2f9438ac3d73e90287db25a357e2034204c58
zhangxueren <zhangxueren@digitforce.com>
2022-08-17 10:19:45 +0800

    增加构建

--- src/source/hive/to_csv/main.py
+++ src/source/hive/to_csv/main.py
@@ -1,6 +1,7 @@
 import logging
 import sys
 
+from digitforce.aip.common.file_helper import create_dir
 from digitforce.aip.common.hive_helper import df_hive_helper
 
 
@@ -11,6 +12,7 @@ def main():
     file_path = sys.argv[2]
     logging.info(sql)
     df = df_hive_helper.query_to_df(sql)
+    create_dir(file_path)
     df.to_csv(file_path, index=False)
 
 
