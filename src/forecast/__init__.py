00b2f959dc855a81c4eb462d6b1505c3156db2b4
王同贵 <wangtonggui@digitforce.com>
2022-09-04 15:34:39 +0800

    删除无用的代码，后续将从0.2.x分支进行增量合并

--- src/forecast/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-# @Time : 2021/12/25
-# @Author : Arvin
--- src/forecast/data_analysis/pd/goods_pe_usability_pd.py
+++
@@ -1,3 +0,0 @@
-
-def get_goods_pe_usable(pe, pc='pe', rc='r2', pt=-1, rt=0.5):
-    return pe[(pe[pc] <= pt) & (pe[rc] >= rt)]
