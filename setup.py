25f36be755b0de46dbdae124c1c65af1bc63744a
王同贵 <wangtonggui@digitforce.com>
2022-09-04 23:01:37 +0800

    1.添加算法开发与交付的基础镜像dockerfile 2.添加aip打包

--- setup.py
+++ setup.py
@@ -1,19 +1,46 @@
-import os
 from setuptools import setup, find_packages
 
-__version__ = '0.1.0-alpha'
-with open('requirements.txt', 'r') as f:
-    requirements = f.readlines()
+__version__ = '0.1.10'
+requirements = [
+    'pandas',
+    'scipy',
+    'PyHDFS',
+    'redis',
+    'kafka-python',
+    'requests',
+    'Cython',
+    'fonttools',
+    'pyaml',
+    'python-dateutil',
+    'xlrd',
+    'findspark',
+    'h5py',
+    'hdfs',
+    'lxml',
+    'Pillow',
+    'ortools',
+    'openpyxl',
+    'networkx',
+    'kfp==1.0.4',
+    'pysnowflake',
+]
 
 setup(
-    name='aip_sdk',
+    name='digitforce-aip',
     version=__version__,
     author='wangtonggui',
     author_email='wangtonggui@digitforce.com',
     description='算法平台sdk',
-    packages=find_packages(include=('common',)),
-    python_requires='>=3.5.0',
+    packages=find_packages(
+        include=[
+            'digitforce',
+            'digitforce.*',
+        ]),
+    python_requires='>=3.6.0',
     install_requires=requirements
 )
 
-# 执行python3 setup.py install进行本地包构建
+# 参考：https://help.coding.net/docs/artifacts/quick-start/pypi.html
+# 安装依赖包：pip install twine setuptools wheel -i https://pypi.tuna.tsinghua.edu.cn/simple
+# 编译与安装：python3 setup.py sdist bdist_wheel
+# 上传制品库：twine upload -r coding-pypi dist/*
