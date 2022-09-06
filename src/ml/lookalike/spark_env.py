5fdd1d04880522f0d18ecdc9eb633e5f9e71dab8
wh <cuebwanghan@163.com>
2022-09-06 19:32:08 +0800

    修改获取starrocks表的方式

--- src/ml/lookalike/spark_env.py
+++ src/ml/lookalike/spark_env.py
@@ -8,11 +8,14 @@
 import os
 import logging
 from digitforce.aip.common.utils.spark_helper import SparkClient
+import digitforce.aip.common.utils.config_helper as config_helper
+starrocks_config = config_helper.get_module_config("starrocks")
 
 
 class SparkEnv:
     def __init__(self, name):
-        self.spark = SparkClient(name).get_session()
+        self.sparkClient = SparkClient(name)
+        self.spark = self.sparkClient.get_session()
         # self.spark = (SparkSession.builder.appName(name)
         #               .master("yarn")
         #               .config("spark.yarn.dist.archives", "/data/anaconda3/envs/pjy-pyspark3.6.zip#pjy-pyspark3.6")
@@ -42,14 +45,11 @@ class SparkEnv:
         self.spark.sql("set spark.sql.hive.mergeFiles=true")
 
 
-def spark_read(spark, table_name, tempViewName, partitionField, minTime, maxTime):
-    # df = spark.read.format('starrocks').option('starrocks.table.identifier', f'{table_name}').option(
-    #     'starrocks.fenodes', 'bigdata-server-07:8030').option('user', 'root').option('password', '').load()
-    df = spark.read.format('jdbc').option('dbtable', f'{table_name}').option(
-        'url', 'jdbc:mysql://172.21.32.16:9030').option('user', 'root').option('password', '').load()
+def spark_read(sparkClient, table_name, tempViewName, partitionField, minTime, maxTime):
+    df = sparkClient.get_starrocks_table_df(table_name)
     df.filter((df[partitionField] >= minTime) & (df[partitionField] <= maxTime)).write.saveAsTable(tempViewName,
                                                                                                    mode="overwrite")
-    logging.info(f'success creating temp hive table: {tempViewName}')
+    print(f'success creating temp hive table: {tempViewName}')
 
 
 if __name__ == '__main__':
