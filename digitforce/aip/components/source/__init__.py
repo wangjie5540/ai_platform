68b54651fb45b1bd59ed186f29e4a685c2412d2a
王同贵 <wangtonggui@digitforce.com>
2022-12-15 08:20:47 +0800

    1.修改hdfs的host 2.添加read_table的组件声明

---
+++ digitforce/aip/components/source/__init__.py
@@ -0,0 +1,16 @@
+import kfp.dsl as dsl
+import digitforce.aip.common.constants.global_constant as global_constant
+
+
+class ReadTable(dsl.ContainerOp):
+    """
+    数据源-读取表组件
+    """
+    def __init__(self, global_params, name='source.read_table'):
+        super(ReadTable, self).__init__(
+            name=name,
+            image='digit-force-docker.pkg.coding.net/ai-platform/ai-components/source-read_table',
+            command=['python', 'main.py'],
+            arguments=['--global_params', global_params, '--name', name],
+            file_outputs={"out": global_constant.JSON_OUTPUT_PATH}
+        )
--- src/ml/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3
-# encoding: utf-8
-'''
-@file: __init__.py.py
-@time: 2022/12/12 11:28
-@desc:
-'''
