e023aa0b57b334d037588304c05dc0e626d1dbb9
王同贵 <wangtonggui@digitforce.com>
2022-12-18 11:39:24 +0800

    添加sample组件的声明

---
+++ digitforce/aip/components/sample/__init__.py
@@ -0,0 +1,19 @@
+# coding: utf-8
+import kfp.dsl as dsl
+import digitforce.aip.common.constants.global_constant as global_constant
+
+output_name = 'out'
+
+
+class SampleSelectionLookalike(dsl.ContainerOp):
+    """
+    数据源-读取表组件
+    """
+    def __init__(self, name, global_params, data_input):
+        super(SampleSelectionLookalike, self).__init__(
+            name=name,
+            image=f'digit-force-docker.pkg.coding.net/ai-platform/ai-components/sample-sample_selection_lookalike',
+            command=['python', 'main.py'],
+            arguments=['--global_params', global_params, '--data_input', data_input],
+            file_outputs={output_name: global_constant.JSON_OUTPUT_PATH}
+        )
