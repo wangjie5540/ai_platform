3373e0b910b7017de821863a1d6f6413c2bd847d
王同贵 <wangtonggui@digitforce.com>
2022-12-22 21:01:30 +0800

    1.优化spark-helper配置 2.添加dockerfile文件

--- digitforce/aip/common/utils/spark_helper.py
+++ digitforce/aip/common/utils/spark_helper.py
@@ -24,14 +24,13 @@ class SparkClient(object):
             .config("spark.driver.host", client_host) \
             .config("spark.kubernetes.container.image", spark_config['kubernetes_runtime_image']) \
             .config("spark.sql.autoBroadcastJoinThreshold", -1) \
-            .config("spark.executor.instances", "10") \
+            .config("spark.executor.instances", "4") \
             .config("spark.debug.maxToStringFields", 100)\
-            .config("spark.executor.cores", "2")\
-            .config("spark.executor.memory", "10g")\
-            .config("spark.driver.memory", "10g")\
-            .config("spark.driver.cores", "2") \
-            .config("spark.driver.maxResultSize", "8g") \
-            .config("spark.sql.shuffle.partitions", "600")\
+            .config("spark.executor.cores", "1")\
+            .config("spark.executor.memory", "6g")\
+            .config("spark.driver.memory", "6g")\
+            .config("spark.driver.cores", "1") \
+            .config("spark.driver.maxResultSize", "4g") \
             .config("hive.metastore.uris", spark_config['hive_uris']) \
             .enableHiveSupport().getOrCreate()
 
