0c9ba5541b65b969c90b13a2402e93f6073b9f20
王同贵 <wangtonggui@digitforce.com>
2022-12-27 16:22:31 +0800

    去掉对base_component的设计与使用

--- digitforce/aip/components/feature_engineering/__init__.py
+++ digitforce/aip/components/feature_engineering/__init__.py
@@ -2,16 +2,11 @@
 import kfp.dsl as dsl
 
 import digitforce.aip.common.utils.component_helper as component_helper
-from digitforce.aip.components.base_component import BaseComponent
-
-user_feature_name = "user_feature"
-item_feature_name = "item_feature"
 
 
 class FeatureCreateLookalike(dsl.ContainerOp):
-    """
-    数据源-读取表组件
-    """
+    OUTPUT_USER_FEATURE = 'user_feature'
+    OUTPUT_ITEM_FEATURE = 'item_feature'
 
     def __init__(self, name, global_params, sample):
         super(FeatureCreateLookalike, self).__init__(
@@ -20,41 +15,41 @@ class FeatureCreateLookalike(dsl.ContainerOp):
             command=['python', 'main.py'],
             arguments=['--name', name, '--global_params', global_params, '--sample', sample],
             file_outputs={
-                user_feature_name: component_helper.generate_output_path(user_feature_name),
-                item_feature_name: component_helper.generate_output_path(item_feature_name)
+                self.OUTPUT_USER_FEATURE: component_helper.generate_output_path(self.OUTPUT_USER_FEATURE),
+                self.OUTPUT_ITEM_FEATURE: component_helper.generate_output_path(self.OUTPUT_ITEM_FEATURE)
             }
         )
 
 
-class RawUserFeatureCalculateOp(dsl.ContainerOp, BaseComponent):
-    def __init__(self, name, global_params, raw_user_feature_table_name):
-        super(RawUserFeatureCalculateOp, self).__init__(
+class RawUserFeatureOp(dsl.ContainerOp):
+    OUTPUT_RAW_USER_FEATURE = 'raw_user_feature'
+
+    def __init__(self, name, global_params):
+        super(RawUserFeatureOp, self).__init__(
             name=name,
             image=f'digit-force-docker.pkg.coding.net/'
                   f'ai-platform/ai-components/feature_engineering-raw_user_feature',
-            command=['python', 'main.py'],
-            arguments=['--raw_user_feature_table_name', raw_user_feature_table_name],
+            arguments=[
+                '--name', name,
+                '--global_params', global_params,
+            ],
             file_outputs={
-                raw_user_feature_table_name: component_helper.generate_output_path(raw_user_feature_table_name)
+                self.OUTPUT_RAW_USER_FEATURE: component_helper.generate_output_path(self.OUTPUT_RAW_USER_FEATURE)
             }
         )
-        super(RawUserFeatureCalculateOp, self).init_aip_container()
-        super(RawUserFeatureCalculateOp, self).add_env_variable("global_params", global_params)
-        super(RawUserFeatureCalculateOp, self).add_env_variable("name", name)
 
 
-class RawItemFeatureCalculateOp(dsl.ContainerOp, BaseComponent):
-    def __init__(self, name, global_params, raw_item_feature_table_name):
-        super(RawItemFeatureCalculateOp, self).__init__(
+class RawItemFeatureOp(dsl.ContainerOp):
+    OUTPUT_RAW_ITEM_FEATURE = 'raw_item_feature'
+
+    def __init__(self, name, global_params):
+        super(RawItemFeatureOp, self).__init__(
             name=name,
             image=f'digit-force-docker.pkg.coding.net/'
                   f'ai-platform/ai-components/feature_engineering-raw_item_feature',
             command=['python', 'main.py'],
-            arguments=['--raw_item_feature_table_name', raw_item_feature_table_name],
+            arguments=['--name', name, '--global_params', global_params],
             file_outputs={
-                raw_item_feature_table_name: component_helper.generate_output_path(raw_item_feature_table_name)
+                self.OUTPUT_RAW_ITEM_FEATURE: component_helper.generate_output_path(self.OUTPUT_RAW_ITEM_FEATURE)
             }
         )
-        super(RawItemFeatureCalculateOp, self).init_aip_container()
-        super(RawItemFeatureCalculateOp, self).add_env_variable("global_params", global_params)
-        super(RawItemFeatureCalculateOp, self).add_env_variable("name", name)
