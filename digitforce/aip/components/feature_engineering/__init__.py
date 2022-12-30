768e13511a2d3acda354a4fbb43897bd6fb01135
zhangxueren <zhangxueren@digitforce.com>
2022-12-30 18:06:19 +0800

    tmp

--- digitforce/aip/components/feature_engineering/__init__.py
+++ digitforce/aip/components/feature_engineering/__init__.py
@@ -9,10 +9,11 @@ class FeatureCreateLookalike(dsl.ContainerOp):
     OUTPUT_ITEM_FEATURE = 'item_feature'
 
     def __init__(self, name, global_params, sample):
+        image_name = f'digit-force-docker.pkg.coding.net/ai-platform/ai-components/' \
+                     f'feature_engineering-feature_create_lookalike'
         super(FeatureCreateLookalike, self).__init__(
+            image=image_name,
             name=name,
-            image=f'digit-force-docker.pkg.coding.net/ai-platform/ai-components/feature_engineering-feature_create_lookalike',
-            command=['python', 'main.py'],
             arguments=['--name', name, '--global_params', global_params, '--sample', sample],
             file_outputs={
                 self.OUTPUT_USER_FEATURE: component_helper.generate_output_path(self.OUTPUT_USER_FEATURE),
@@ -44,6 +45,22 @@ class RawItemFeatureOp(dsl.ContainerOp):
 
     def __init__(self, name, global_params):
         super(RawItemFeatureOp, self).__init__(
+            name=name,
+            image=f'digit-force-docker.pkg.coding.net/'
+                  f'ai-platform/ai-components/feature_engineering-raw_item_feature',
+            command=['python', 'main.py'],
+            arguments=['--name', name, '--global_params', global_params],
+            file_outputs={
+                self.OUTPUT_RAW_ITEM_FEATURE: component_helper.generate_output_path(self.OUTPUT_RAW_ITEM_FEATURE)
+            }
+        )
+
+
+class ModelItemFeatureOp(dsl.ContainerOp):
+    OUTPUT_RAW_ITEM_FEATURE = 'raw_item_feature'
+
+    def __init__(self, name, global_params):
+        super(ModelItemFeatureOp, self).__init__(
             name=name,
             image=f'digit-force-docker.pkg.coding.net/'
                   f'ai-platform/ai-components/feature_engineering-raw_item_feature',
