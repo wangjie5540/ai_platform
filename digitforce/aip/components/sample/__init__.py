af4139c8186fe3316a5cfaf28075434869f05c49
zhangxueren <zhangxueren@digitforce.com>
2023-01-03 14:04:52 +0800

    tmp

--- digitforce/aip/components/sample/__init__.py
+++ digitforce/aip/components/sample/__init__.py
@@ -12,7 +12,8 @@ class SampleSelectionLookalike(dsl.ContainerOp):
     def __init__(self, name, global_params):
         super(SampleSelectionLookalike, self).__init__(
             name=name,
-            image=f'digit-force-docker.pkg.coding.net/ai-platform/ai-components/sample-sample_selection_lookalike',
+            image=f'digit-force-docker.pkg.coding.net/ai-platform/ai-components/'
+                  f'src-sample-sample_selection_lookalike',
             command=['python', 'main.py'],
             arguments=['--name', name, '--global_params', global_params],
             file_outputs={output_name: component_helper.generate_output_path(output_name)}
