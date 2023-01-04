3775dd83639bbda3436cbc706c4e4d8d985e15c3
王同贵 <wangtonggui@digitforce.com>
2023-01-04 11:11:48 +0800

    添加编译组件的脚本，用于自动集成

--- scripts/build_component.py
+++ scripts/build_component.py
@@ -1,17 +1,14 @@
 # coding:utf-8
 
-import argparse
 import os
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--component_name', type=str, required=True, help='组件名称')
-    args = parser.parse_args()
-    component_path = os.path.join('src', *args.component_name.split('-'))
+    component_name = os.environ['COMPONENT_NAME']
+    component_path = os.path.join('src', *component_name.split('-'))
     print(component_path)
     os.system(
-        f"docker build -t digit-force-docker.pkg.coding.net/ai-platform/ai-components/{args.component_name} {component_path}")
+        f"docker build -t digit-force-docker.pkg.coding.net/ai-platform/ai-components/{component_name} {component_path}")
 
 
 if __name__ == '__main__':
