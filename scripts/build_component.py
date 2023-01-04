1320be53481e8301c40889b434625fd072f4d27b
王同贵 <wangtonggui@digitforce.com>
2023-01-04 13:38:10 +0800

    调试

--- scripts/build_component.py
+++ scripts/build_component.py
@@ -10,8 +10,13 @@ def main():
     os.chdir(component_path)
     cmd = "docker build -t digit-force-docker.pkg.coding.net/ai-platform/ai-components/{component_name} .".format(
         component_name=component_name)
-    print()
     subprocess.check_call(cmd, shell=True)
+    subprocess.check_call(
+        "docker login -u ai-components-1672810563540 -p 1d228954e03793ce2e79bf655335abc4e961ec75 digit-force-docker.pkg.coding.net",
+        shell=True)
+    subprocess.check_call(
+        "docker push digit-force-docker.pkg.coding.net/ai-platform/ai-components/{component_name}".format(
+            component_name=component_name), shell=True)
 
 
 if __name__ == '__main__':
