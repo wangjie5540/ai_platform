25de052139ae1de28253b05ffdf45aa2c0c5e6f5
王同贵 <wangtonggui@digitforce.com>
2023-01-04 11:37:19 +0800

    调试

--- scripts/build_component.py
+++ scripts/build_component.py
@@ -7,12 +7,10 @@ import subprocess
 def main():
     component_name = os.environ['COMPONENT_NAME']
     component_path = os.path.join('src', *component_name.split('-'))
-    print(component_path)
     os.chdir(component_path)
     command = "docker build -t digit-force-docker.pkg.coding.net/ai-platform/ai-components/{component_name} .".format(
         component_name=component_name)
-    print(command)
-    # subprocess.check_call(command, shell=True)
+    subprocess.check_call(command, shell=True)
 
     if __name__ == '__main__':
         main()
