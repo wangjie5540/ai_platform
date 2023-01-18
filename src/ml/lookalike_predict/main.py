a51dd1f8fe2cc076e2ed38c6bc6e159ce6b9c50a
zhangxueren <zhangxueren@digitforce.com>
2023-01-18 18:37:29 +0800

    fix bug

--- src/ml/lookalike_predict/main.py
+++ src/ml/lookalike_predict/main.py
@@ -9,7 +9,6 @@ def run():
     # 参数解析
     df_argument_helper.add_argument("--global_params", type=str, required=False, help="全局参数")
     df_argument_helper.add_argument("--name", type=str, required=False, help="name")
-    df_argument_helper.add_argument("--user_vec_table_name", type=str, required=False, help="用户向量表")
     df_argument_helper.add_argument("--seeds_crowd_table_name", type=str, required=False, help="种子人群")
     df_argument_helper.add_argument("--predict_crowd_table_name", type=str, required=False, help="待扩散人群")
     df_argument_helper.add_argument("--output_file_name", type=str, required=False, help="输出文件名称")
@@ -18,7 +17,6 @@ def run():
     seeds_crowd_table_name = df_argument_helper.get_argument("seeds_crowd_table_name")
     predict_crowd_table_name = df_argument_helper.get_argument("predict_crowd_table_name")
     output_file_name = df_argument_helper.get_argument("output_file_name")
-
     print(f"user_vec_table_name:{user_vec_table_name}")
     print(f"seeds_crowd_table_name:{seeds_crowd_table_name}")
     print(f"predict_crowd_table_name:{predict_crowd_table_name}")
