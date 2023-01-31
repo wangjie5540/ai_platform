d5e647a2b672bd2b808613882a39884a8700f2b3
zhangxueren <zhangxueren@digitforce.com>
2023-01-31 14:05:16 +0800

    fix bug

--- digitforce/aip/common/aip_feature/zq_feature.py
+++ digitforce/aip/common/aip_feature/zq_feature.py
@@ -1,4 +1,130 @@
-from digitforce.aip.common.aip_feature.feature import *
+import logging
+import os
+import os.path
+import pickle
+import uuid
+
+from digitforce.aip.common.utils.hdfs_helper import hdfs_client
+from digitforce.aip.common.utils.hive_helper import hive_client
+
+
+class FeatureEncoder(object):
+    def __init__(self, name, version, default=None, source_table_name=None):
+        self.source_table_name = source_table_name
+        self.name = name
+        self.version = version
+        self.default = default
+
+    def get_model_feature_value(self, value):
+        return self.default
+
+
+class CategoryFeatureEncoder(FeatureEncoder):
+    def __init__(self, name, version, default=None, source_table_name=None):
+        super(CategoryFeatureEncoder, self).__init__(name, version, default, source_table_name)
+        self.vocabulary = {}
+
+    def get_model_feature_value(self, value):
+        if not isinstance(value, str):
+            logging.warning(f"the category feature is wanted as string but is {type(value)} {value}")
+            value = str(value)
+        return self.vocabulary.get(value, self.default)
+
+
+class NumberFeatureEncoder(FeatureEncoder):
+    def __init__(self, name, version, default=None, source_table_name=None):
+        super(NumberFeatureEncoder, self).__init__(name, version, default, source_table_name)
+        self.default = float(self.default)
+        self.mean = None
+        self.std = None
+
+    def get_model_feature_value(self, value):
+        if value in [None, "null", "", "None", "NULL"]:
+            return float(self.default)
+        try:
+            result = (value - self.mean) / self.std
+            return result
+        except Exception as e:
+            logging.error(e)
+            return self.default
+
+
+class FeatureEncoderCalculator:
+
+    @classmethod
+    def calculate(cls, encoder):
+        pass
+
+    @classmethod
+    def get_save_path(cls, encoder):
+        return "/user/ai/aip/zq/model_feature/v1" + f"/{encoder.source_table_name}/{encoder.name}/{encoder.version}"
+
+    @classmethod
+    def save_to_hdfs(cls, encoder: FeatureEncoder):
+        hdfs_path = cls.get_save_path(encoder)
+        hdfs_client.mkdir_dirs(os.path.dirname(hdfs_path))
+        result = pickle.dumps(encoder)
+        hdfs_client.write_to_hdfs(result, hdfs_path)
+
+    @classmethod
+    def read_from_hdfs(cls, encoder):
+        hdfs_path = cls.get_save_path(encoder)
+        tmp_file = f"tmp-{uuid.uuid4()}"
+        hdfs_client.copy_to_local(hdfs_path, tmp_file)
+        with open(tmp_file, "rb") as fi:
+            pickle_encoder = pickle.load(fi)
+        os.remove(tmp_file)
+        return pickle_encoder
+
+    @classmethod
+    def load_encoder(cls, encoder: FeatureEncoder, use_hdfs=True):
+        if hdfs_client.exists(cls.get_save_path(encoder)) and use_hdfs:
+            encoder = cls.read_from_hdfs(encoder)
+            return encoder
+        logging.info(f"begin calculate feature encoder: {encoder.name} version:{encoder.version} "
+                     f"source_table_name:{encoder.source_table_name}")
+        cls.calculate(encoder)
+        cls.save_to_hdfs(encoder)
+
+
+class CategoryFeatureEncoderCalculator(FeatureEncoderCalculator):
+    @classmethod
+    def calculate(cls, encoder: CategoryFeatureEncoder):
+        query_sql = f'''
+                            SELECT 
+                                DISTINCT {encoder.name}  AS {encoder.name}
+                            FROM {encoder.source_table_name}
+                    '''
+        df = hive_client.query_to_df(query_sql)
+        encoder.vocabulary = dict([(_, i + 1) for i, _ in enumerate(df[encoder.name].astype(str))])
+
+    @classmethod
+    def read_from_hdfs(cls, encoder: CategoryFeatureEncoder):
+        _encoder = super().read_from_hdfs(encoder)
+        encoder.vocabulary = _encoder.vocabulary
+        return encoder
+
+
+class NumberFeatureEncoderCalculator(FeatureEncoderCalculator):
+    @classmethod
+    def calculate(cls, encoder: NumberFeatureEncoder):
+        query_sql = f'''
+                            SELECT 
+                                SUM({encoder.name}) / COUNT(*)  AS mean_res,
+                                STDDEV({encoder.name}) AS stddev_res 
+                            FROM {encoder.source_table_name}
+                    '''
+        df = hive_client.query_to_df(query_sql)
+        encoder.mean = df["mean_res"].tolist()[0]
+        encoder.std = df["stddev_res"].tolist()[0]
+        return encoder
+
+    @classmethod
+    def read_from_hdfs(cls, encoder: NumberFeatureEncoder):
+        _encoder = super().read_from_hdfs(encoder)
+        encoder.std = _encoder.std
+        encoder.mean = _encoder.mean
+        return encoder
 from digitforce.aip.common.utils.time_helper import *
 
 USER_RAW_FEATURE_TABLE_NAME = "algorithm.tmp_raw_user_feature_table_name"
