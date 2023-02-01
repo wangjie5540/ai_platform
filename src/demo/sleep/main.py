803d7e9f17e391d9e54da3288d0ec0d8f7ad3c4e
王同贵 <wangtonggui@digitforce.com>
2023-02-01 14:09:17 +0800

    调试sleep组件

--- src/demo/sleep/main.py
+++ src/demo/sleep/main.py
@@ -7,7 +7,7 @@ def run():
     parser = argparse.ArgumentParser()
     parser.add_argument('--minutes', type=str, help='sleep的分钟数')
     args = parser.parse_args()
-    my_sleep.do_sleep(args.param)
+    my_sleep.do_sleep(args.minutes)
 
 
 if __name__ == '__main__':
