4ccea74ed0319d6a6e3f588e8c5d2e5f2999f08a
王同贵 <wangtonggui@digitforce.com>
2023-02-01 19:35:03 +0800

    调试编译组件,添加sleep的demo组件

--- src/demo/sleep/main.py
+++ src/demo/sleep/main.py
@@ -1,24 +1,15 @@
 # coding: utf-8
-import subprocess
-
-cmd = '''
-cp /mnt/config/aip_config.yaml /usr/local/etc
-cp /mnt/config/kube_config /root/.kube/config
-cp /mnt/config/hdfs-site.xml $SPARK_HOME/conf
-'''
-subprocess.check_call(cmd, shell=True)
-
 import argparse
-import digitforce.aip.common.utils.spark_helper as spark_helper
+import digitforce.aip.common.utils.component_helper as component_helper
+import my_sleep
 
 
 def run():
+    component_helper.init_config()
     parser = argparse.ArgumentParser()
     parser.add_argument('--minutes', type=int, help='sleep的分钟数')
     args = parser.parse_args()
-    # my_sleep.do_sleep(args.minutes)
-    df = spark_helper.spark_client.get_starrocks_table_df("algorithm.sample_jcbq_zxr")
-    df.show()
+    my_sleep.do_sleep(args.minutes)
 
 
 if __name__ == '__main__':
