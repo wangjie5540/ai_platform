e03cc46226735e1e555a6acfd29c1babb05727fa
王同贵 <wangtonggui@digitforce.com>
2023-02-01 14:50:10 +0800

    调试sleep组件

--- src/demo/sleep/main.py
+++ src/demo/sleep/main.py
@@ -1,13 +1,16 @@
 # coding: utf-8
 import my_sleep
 import argparse
+import digitforce.aip.common.utils.spark_helper as spark_helper
 
 
 def run():
     parser = argparse.ArgumentParser()
     parser.add_argument('--minutes', type=int, help='sleep的分钟数')
     args = parser.parse_args()
-    my_sleep.do_sleep(args.minutes)
+    # my_sleep.do_sleep(args.minutes)
+    df = spark_helper.spark_client.get_starrocks_table_df("algorithm.sample_jcbq_zxr")
+    df.show()
 
 
 if __name__ == '__main__':
