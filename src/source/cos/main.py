7576686a8227a58225dbdb5d4f0f76c985a4bc5f
王同贵 <wangtonggui@digitforce.com>
2023-02-02 11:34:22 +0800

    修复cos数据源组件声明的异常

--- src/source/cos/main.py
+++ src/source/cos/main.py
@@ -10,10 +10,10 @@ def run():
     parser = argparse.ArgumentParser()
     parser.add_argument('--name', type=str, required=True, help='名称')
     parser.add_argument('--global_params', type=str, required=True, help='pipeline全局参数')
-    parser.add_argument('--columns', type=str, required=True, help='列名，使用逗号分隔')
     args = parser.parse_args()
     url = json.loads(args.global_params)[args.name]['url']
-    table_name, columns = read_cos.read_to_table(url, args.columns)
+    columns = json.loads(args.global_params)[args.name]['columns']
+    table_name, columns = read_cos.read_to_table(url, columns)
     outputs = {
         "type": "hive_table",
         "table_name": table_name,
