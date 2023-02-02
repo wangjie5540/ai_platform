900e7b5c4b4e6e61438613b359565d9da1f1526c
王同贵 <wangtonggui@digitforce.com>
2023-02-02 11:22:08 +0800

    修复cos数据源组件声明的异常

--- digitforce/aip/components/source/cos.py
+++ digitforce/aip/components/source/cos.py
@@ -6,16 +6,15 @@ import digitforce.aip.common.utils.component_helper as component_helper
 class Cos(BaseComponent):
     OUTPUT_1 = 'table_name'
 
-    def __init__(self, name: str, url: str, columns: str, tag='latest'):
+    def __init__(self, name: str, global_params: str, tag='latest'):
         """
         :param name: 名称
-        :param url: 目标文件地址(例如：https://bucket-name/path/to/file)
-        :param columns: 列名，使用逗号分隔
+        :param global_params: pipeline全局参数
         """
         super().__init__(
             name,
             image=f"{global_constant.AI_PLATFORM_IMAGE_REPO}/source-cos",
-            arguments=['--url', url, '--columns', columns],
+            arguments=['--name', name, '--global_params', global_params],
             file_outputs={
                 Cos.OUTPUT_1: component_helper.generate_output_path(Cos.OUTPUT_1)
             },
