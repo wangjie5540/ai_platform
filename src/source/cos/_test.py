464a91983a0512e5df61dbbc81412f257e95ad0d
wh <wanghan@digitforce.com>
2023-02-08 15:35:45 +0800

    删除部分无用文件

--- src/source/cos/_test.py
+++ src/source/cos/_test.py
@@ -1,9 +1,3 @@
 import read_cos
 
-table_name, column_list = read_cos.read_to_table("https://algorithm-1308011215.cos.ap-beijing.myqcloud.com/aip_test_lookalike_predict.csv", "user_id")
-print(table_name)
-print(column_list)
-# https://algorithm-1308011215.cos.ap-beijing.myqcloud.com/aip_test_lookalike_seeds.csv
-# # aip.cos_4717365493815578625
-# https://algorithm-1308011215.cos.ap-beijing.myqcloud.com/aip_test_lookalike_predict.csv
-# # aip.cos_4717597372074430465
+read_cos.read_to_table("http://algorithm-1308011215.cos.ap-beijing.myqcloud.com/result.csv", "a,b")
