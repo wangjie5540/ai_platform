076c3fbb280f8da3ce055f257da4a253f48a6900
王同贵 <wangtonggui@digitforce.com>
2023-02-13 10:28:06 +0800

    调试组件的dockerfile已经存在的情况

--- scripts/build_component.py
+++ scripts/build_component.py
@@ -3,6 +3,10 @@
 import os
 import subprocess
 
+"""
+本脚本仅用于coding上的CI构建组件(尚未在Windows上测试)
+"""
+
 component_list = [
     'demo-print',
 ]
@@ -25,11 +29,6 @@ def build_algorithm_base():
     algorithm_base_dockerfile = '''
 from digit-force-docker.pkg.coding.net/ai-platform/base-images/algorithm-base:latest
 RUN pip install digitforce-aip -i https://aip-1657964384920:546b044f44ad6936fef609faa512a53b3fa8b12f@digit-force-pypi.pkg.coding.net/ai-platform/aip/simple
-# 添加配置挂载映射
-# 参考: https://spark.apache.org/docs/latest/running-on-kubernetes.html#configuration
-# ENV KUBECONFIG /usr/local/etc/kube_config
-# 参考: https://spark.apache.org/docs/latest/configuration.html
-# ENV SPARK_CONF_DIR /usr/local/etc
 '''
     with open('Dockerfile', 'w') as f:
         f.write(algorithm_base_dockerfile)
@@ -44,25 +43,33 @@ RUN pip install digitforce-aip -i https://aip-1657964384920:546b044f44ad6936fef6
 
 
 def build_component(base_image, component_name, tag='latest'):
-    component_dockerfile = '''
+    # 获取组件路径
+    component_path = os.path.join('src', *component_name.split('-'))
+    # 获取编译命令
+    component_image = "digit-force-docker.pkg.coding.net/ai-platform/ai-components/{component_name}:{tag}".format(
+        component_name=component_name, tag=tag)
+    # 查询是否存在自定制的Dockerfile，如果存在则使用自定义的Dockerfile
+    customized_dockerfile = os.path.join(component_path, 'Dockerfile')
+    if not os.path.exists(customized_dockerfile):
+        component_dockerfile = '''
 from {base_image}
 ARG COMPONENT_DIR=/component
 RUN mkdir -p $COMPONENT_DIR
 WORKDIR $COMPONENT_DIR
 COPY . $COMPONENT_DIR
-'''.format(base_image=base_image)
-    with open('Dockerfile', 'w') as f:
-        f.write(component_dockerfile)
-    component_path = os.path.join('src', *component_name.split('-'))
-    component_image = "digit-force-docker.pkg.coding.net/ai-platform/ai-components/{component_name}:{tag}".format(
-        component_name=component_name, tag=tag)
-    build_cmd = '''
-docker build -t {component_image} -f {cur_dir}/Dockerfile {component_path}
+    '''.format(base_image=base_image)
+        with open(customized_dockerfile, 'w') as f:
+            f.write(component_dockerfile)
+    print(build_component_cmd(component_image, component_path))
+    subprocess.check_call(build_component_cmd(component_image, component_path), shell=True)
+
+
+def build_component_cmd(component_image, component_path):
+    return '''
+docker build -t {component_image} -f {component_path}/Dockerfile {component_path}
 docker login -u ai-components-1672810563540 -p 1d228954e03793ce2e79bf655335abc4e961ec75 digit-force-docker.pkg.coding.net
 docker push {component_image}
-'''.format(component_image=component_image, cur_dir=os.curdir, component_path=component_path)
-    print(build_cmd)
-    subprocess.check_call(build_cmd, shell=True)
+    '''.format(component_image=component_image, component_path=component_path)
 
 
 if __name__ == '__main__':
