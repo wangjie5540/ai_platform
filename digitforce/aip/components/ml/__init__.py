426fde6b31cd71c9aa0998c2f03b63ff5022e04e
汤思璇 <tangsixuan@digitforce.com>
2023-02-16 19:39:02 +0800

    安装sklearn包

--- digitforce/aip/components/ml/__init__.py
+++ digitforce/aip/components/ml/__init__.py
@@ -2,7 +2,7 @@
 import kfp.dsl as dsl
 
 from digitforce.aip.common.constants.global_constant import ENV
-
+from digitforce.aip.components import BaseComponent
 
 class Lookalike(dsl.ContainerOp):
     def __init__(self, name, global_params, train_dataset, test_dataset, tag=ENV):
@@ -79,26 +79,26 @@ class LiushiPredict(dsl.ContainerOp):
             }
         )
 
-class GaoqianModel(dsl.ContainerOp):
+class GaoqianModel(BaseComponent):
     def __init__(self, name, global_params, train_data, test_data, tag=ENV):
         super(GaoqianModel, self).__init__(
             name=name,
             image=f'digit-force-docker.pkg.coding.net/ai-platform/ai-components/'
-                  f'ml-gaoqian:{tag}',
-            command=['python', 'main.py'],
+                  f'ml-gaoqian',
+            tag=tag,
             arguments=['--name', name, '--global_params', global_params, '--train_data', train_data,
                        '--test_data', test_data],
             file_outputs={
             }
         )
 
-class GaoqianPredict(dsl.ContainerOp):
+class GaoqianPredict(BaseComponent):
     def __init__(self, name, global_params, predict_table_name, tag=ENV):
         super(GaoqianPredict, self).__init__(
             name=name,
             image=f'digit-force-docker.pkg.coding.net/ai-platform/ai-components/'
-                  f'ml-gaoqian_predict:{tag}',
-            command=['python', 'main.py'],
+                  f'ml-gaoqian_predict',
+            tag=tag,
             arguments=['--name', name, '--global_params', global_params, '--predict_table_name', predict_table_name,
                        ],
             file_outputs={
