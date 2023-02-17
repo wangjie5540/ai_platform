8f432bc30806557d30e6623f05a22a6810f07a41
汤思璇 <tangsixuan@digitforce.com>
2023-02-17 11:00:12 +0800

    修改预测模型构造参数读取

--- src/ml/gaoqian_predict/main.py
+++ src/ml/gaoqian_predict/main.py
@@ -12,24 +12,22 @@ from digitforce.aip.common.utils.argument_helper import df_argument_helper
 def run():
     # 参数解析
     # component_helper.init_config()
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--global_params", type=str, required=True, help="全局参数")
-    parser.add_argument("--name", type=str, required=True, help="名称")
-    parser.add_argument("--sample", type=str, required=False, help="预测数据")
-    # df_argument_helper.add_argument("--model_hdfs_path", type=str, required=False, help="模型地址")
-    # df_argument_helper.add_argument("--output_file_name", type=str, required=False, help="输出文件名称")
-    args = parser.parse_args()
-    global_params = json.loads(args.global_params)
-    predict_table_name_params = json.loads(args.sample)
-    predict_table_name = predict_table_name_params['table_name']
-    model_hdfs_path = global_params[args.name]["model_hdfs_path"]
-    output_file_name = global_params[args.name]["output_file_name"]
+    df_argument_helper.add_argument("--global_params", type=str, required=True, help="全局参数")
+    df_argument_helper.add_argument("--name", type=str, required=True, help="名称")
+    df_argument_helper.add_argument("--predict_table_name", type=str, required=False, help="预测数据")
+    df_argument_helper.add_argument("--model_hdfs_path", type=str, required=False, help="模型地址")
+    df_argument_helper.add_argument("--output_file_name", type=str, required=False, help="输出文件名称")
+
+    predict_table_name = df_argument_helper.get_argument("predict_table_name")
+    model_hdfs_path = df_argument_helper.get_argument("model_hdfs_path")
+    output_file_name = df_argument_helper.get_argument("output_file_name")
     print(f"predict_table_name:{predict_table_name}")
     print(f"model_hdfs_path:{model_hdfs_path}")
     print(f"output_file_name:{output_file_name}")
     start_model_predict(predict_table_name, model_hdfs_path, output_file_name)
 
-    outputs = {
+
+outputs = {
 
     }
     # component_helper.write_output(outputs)
