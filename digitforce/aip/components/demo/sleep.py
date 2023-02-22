4dc4ab559db0292d812e5a64fc03ae9b4b031e9c
王同贵 <wangtonggui@digitforce.com>
2023-02-22 17:25:20 +0800

    添加自检的pipeline

--- digitforce/aip/components/demo/sleep.py
+++ digitforce/aip/components/demo/sleep.py
@@ -1,11 +1,12 @@
 from digitforce.aip.components import BaseComponent
+import digitforce.aip.common.constants.global_constant as global_constant
 
 
-class Sleep(BaseComponent):
-    def __init__(self, name: str, minutes, tag='latest'):
+class DemoSleep(BaseComponent):
+    def __init__(self, name: str, global_params: str, tag='latest'):
         super().__init__(
             name=name,
-            image=f'digit-force-docker.pkg.coding.net/ai-platform/ai-components/demo-sleep',
-            arguments=['--minutes', minutes],
+            image=f'{global_constant.AI_PLATFORM_IMAGE_REPO}/demo-sleep',
+            arguments=['--name', name, '--global_params', global_params],
             tag=tag,
         )
--- digitforce/aip/components/demo/starrocks.py
+++ digitforce/aip/components/demo/starrocks.py
@@ -2,13 +2,11 @@ from digitforce.aip.components import BaseComponent
 import digitforce.aip.common.constants.global_constant as global_constant
 
 
-class DemoPrint(BaseComponent):
-    param = None
-
+class DemoStarrocks(BaseComponent):
     def __init__(self, name: str, global_params: str, tag='latest'):
         super().__init__(
             name=name,
-            image=f'{global_constant.AI_PLATFORM_IMAGE_REPO}/demo-print',
+            image=f'{global_constant.AI_PLATFORM_IMAGE_REPO}/demo-starrocks',
             arguments=['--name', name, '--global_params', global_params],
             tag=tag,
         )
