e3ab835543c74faae70319bb7c07c635dcec0219
王同贵 <wangtonggui@digitforce.com>
2023-02-23 17:44:45 +0800

    demo使用新的spark引用方式

--- src/demo/starrocks/read_starrocks.py
+++ src/demo/starrocks/read_starrocks.py
@@ -2,5 +2,5 @@ import digitforce.aip.common.utils.spark_helper as spark_helper
 
 
 def do_read(table_name, limit):
-    spark_client = spark_helper.SparkClient()
+    spark_client = spark_helper.SparkClient.get()
     spark_client.get_starrocks_table_df(table_name).show(limit)
