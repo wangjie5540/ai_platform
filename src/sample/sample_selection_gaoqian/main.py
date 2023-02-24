8de2c2e2ee587a75f5ab9d4d56f8cb0640b778ca
汤思璇 <tangsixuan@digitforce.com>
2023-02-24 13:20:22 +0800

    样本构造-滑动窗口改造

--- src/sample/sample_selection_gaoqian/main.py
+++ src/sample/sample_selection_gaoqian/main.py
@@ -20,16 +20,24 @@ def run():
 
     # todo: 类别中英文映射转换
     category_map = {
-        "hunhe": "混合型",
-        "gupiao": "股票型"
+        "gongmu": "公墓基金",
+        "xintuo": "信托产品"
     }
     category = category_map.get(category)
+
+    event_code_map = {
+        "shengou": "申购",
+        "shuhui": "赎回",
+        "rengou": "认购"
+    }
+    event_code = event_code_map.get(event_code)
+
     # TODO 从参数中获取
-    event_table_name = 'algorithm.zq_fund_trade_lite'
-    event_columns = ['cust_code', 'event_code', 'product_id', 'product_type_pri', 'dt']
-    item_table_name = 'algorithm.zq_fund_basic'
-    item_columns =['ts_code', 'fund_type']
-    table_name, columns = sample_create(event_table_name, event_columns, item_table_name, item_columns, event_code, category, train_period, predict_period)
+    trade_table_name = 'algorithm.dm_cust_subs_redm_event_df'
+    trade_columns = ['cust_code', 'event_code', 'product_type_pri', 'dt']
+    event_table_name = 'algorithm.dm_cust_traf_behv_aggregate_df'
+    event_columns = ['cust_code', 'is_login', 'dt']
+    table_name, columns = sample_create(trade_table_name, trade_columns, event_table_name, event_columns, event_code, category, train_period, predict_period)
     component_helper.write_output("sample", table_name)
 
 
--- src/sample/sample_selection_gaoqian/sample_select.py
+++ src/sample/sample_selection_gaoqian/sample_select.py
@@ -1,79 +1,67 @@
 import datetime
+import random
+
 import digitforce.aip.common.utils.spark_helper as spark_helper
 import digitforce.aip.common.utils.time_helper as time_helper
-
+from utils import getActiveDays
 
 DATE_FORMAT = "%Y-%m-%d"
 spark_client = spark_helper.SparkClient()
 
-def sample_create(event_table_name, event_columns, item_table_name, item_columns, event_code, category_a, train_period, predict_period):
+def sample_create(trade_table_name, trade_columns, event_table_name, event_columns, event_code, category, train_period, predict_period):
+    window_days = 15
+    # today = time_helper.get_today_str()
+    end_date = time_helper.n_days_ago_str(predict_period + 2)
+    start_date = time_helper.get_date_n_days_ago_str(end_date, window_days, DATE_FORMAT)
 
-    today = time_helper.get_today_str()
-    one_year_ago = time_helper.n_days_ago_str(365)
-    user_id = event_columns[0]
-    item_id = event_columns[2]
-    trade_date = event_columns[-1]
-    trade_type = event_columns[1]
-    # item_id_item = item_columns[0]
-    fund_type = event_columns[3] #item_columns[1]
+    # 数据起始日期：基于start_data, 过去n天， start_date - n
+    active_start_date = time_helper.get_date_n_days_ago_str(start_date, train_period, DATE_FORMAT)
+    # 数据戒指日期；基于end_date, 未来m天， end_date + m
+    buy_end_date = time_helper.get_date_n_days_ago_str(end_date, -predict_period, DATE_FORMAT)
 
     event_data = spark_client.get_starrocks_table_df(event_table_name)
     event_data = event_data.select(event_columns)\
-        .filter((event_data[trade_date] >= one_year_ago) & (event_data[trade_date] < today))
+        .filter((event_data['dt'] >= active_start_date) & (event_data['dt'] < buy_end_date))
 
-    # item_data = spark_client.get_starrocks_table_df(item_table_name)
-    # item_data = item_data.select(item_columns).distinct()
+    trade_data = spark_client.get_starrocks_table_df(trade_table_name)
+    trade_data = trade_data.select(trade_columns)\
+        .filter((trade_data['dt'] >= active_start_date) & (trade_data['dt'] < buy_end_date))
 
-    # join_data = event_data.join(item_data.select([item_id_item, fund_type]), event_data[item_id] == item_data[item_id_item])
+    #1. 样本生成
+    ##1.1 客户号 -> 活跃日期：set(str)
+    user_active_days = event_data.rdd.filter(lambda x: x[1] and x[1] > 0).map(lambda x: (x[0], {x[1]}))\
+        .reduceByKey(lambda a, b: a | b)
+    ## 1.2 客户号 -> 申购日期：set(str)
+    user_buy_days = trade_data.rdd.filter(lambda x: x[1] == event_code and x[2] == category).map(lambda x: (x[0], {x[3]}))\
+        .reduceByKey(lambda a, b: a | b)
 
-    # columns = [user_id, trade_type, trade_date, fund_type]
-    data = event_data #join_data.select(columns)
+    ## 1.3 客户号->(日期，过去n天活跃天数，未来m天申购天数）
+    user_days = user_active_days.join(user_buy_days)\
+        .map(lambda x: (x[0], getActiveDays(x[1][0], x[1][1], active_start_date, buy_end_date, train_period, predict_period)))\
+        .flatMapValues(lambda x: x)
 
-    max_trade_date = data.filter((data[trade_type] == event_code) & (data[fund_type] == category_a))\
-        .select(trade_date).rdd.max()[0]
-    print(max_trade_date)
-    interval = max(train_period, predict_period)
-    date_mid_str = (time_helper.get_day_datetime(max_trade_date, DATE_FORMAT) - datetime.timedelta(days=interval)).strftime(DATE_FORMAT)
-    date_from_str = (datetime.datetime.strptime(date_mid_str, DATE_FORMAT) - datetime.timedelta(days=train_period)).strftime(DATE_FORMAT)
-    date_end_str = (datetime.datetime.strptime(date_mid_str, DATE_FORMAT) + datetime.timedelta(days=predict_period)).strftime(DATE_FORMAT)
+    ## 1.4 客户号 -> (日期， label)
+    ##-过去n天至少活跃一次：
+    ## --未来m天未买入该类产品：label=0
+    ## --未来m天买入该类产品：label=1
+    sample_all = user_days.filter(lambda x: x[1][1] >= 1)\
+        .map(lambda x: (x[0], (x[1][0], 0)) if x[1][1] == 0 else (x[0], (x[1][0], 1)))
 
-    sample_a = data.filter((data[trade_date] >= date_from_str) & (data[trade_date] < date_mid_str))
-    sample_b = data.filter((data[trade_date] >= date_mid_str) & (data[trade_date] < date_end_str))
-
-    # 修改高潜候选人的圈选逻辑：该时间段未购买过该品类的用户->该时间段所有活跃用户
-    sample_a_rdd = sample_a.rdd\
-        .map(lambda x: (x[0], 0))\
-        .reduceByKey(lambda x, y: x + y)
-    sample_b_rdd = sample_b.filter(((sample_a[trade_type] == event_code) & (sample_a[fund_type] == category_a))).rdd\
-        .map(lambda x: (x[0], 0))\
-        .reduceByKey(lambda x, y: x + y)
-    sample_columns = ['user_id', 'label']
-    sample_df = sample_a_rdd.map(lambda x: (x[0], 0))\
-        .union(sample_b_rdd.map(lambda x: (x[0], 1)))\
-        .reduceByKey(lambda x, y: x+y)\
+    ## 1.5 采样
+    all_cnt = sample_all.count()
+    label_cnt = 100000
+    sample_rate = label_cnt * 1.0 / all_cnt
+    # 最终结果：客户号， 日期， label
+    sample_columns = ['user_id', 'date', 'label']
+    sample = sample_all.map(lambda x: (x[0], (x[1], random.random())))\
+        .filter(lambda x: x[1][1] < sample_rate)\
+        .map(lambda x: (x[0], x[1][0][[0]], x[1][0][1]))\
         .toDF(sample_columns)
-    # else:
-    #     sample_a_rdd = sample_a.filter(~(((sample_a[trade_type] == event_code_list[0]) | (sample_a[trade_type] == event_code_list[1])) & (sample_a[fund_type] == category_a))).rdd\
-    #         .map(lambda x: (x[0], 0))\
-    #         .reduceByKey(lambda x, y: x + y)
-    #     sample_b_rdd1 = sample_b.filter(((sample_b[trade_type] == event_code_list[0]) & (sample_b[fund_type] == category_a))).rdd \
-    #         .map(lambda x: (x[0], 0))\
-    #         .reduceByKey(lambda x, y: x + y)
-    #     sample_b_rdd2 = sample_b.filter(((sample_b[trade_type] == event_code_list[1]) & (sample_b[fund_type] == category_a))).rdd\
-    #         .map(lambda x: (x[0], 0))\
-    #         .reduceByKey(lambda x, y: x + y)
-    #     sample_columns = ['user_id', 'label1', 'label2']
-    #     sample_df = sample_a_rdd.map(lambda x: (x[0], (0, 0)))\
-    #         .union(sample_b_rdd1.map(lambda x: (x[0], (1, 0))))\
-    #         .union(sample_b_rdd2.map(lambda x: (x[0], (0, 1))))\
-    #         .reduceByKey(lambda x, y: (x[0] + y[0], x[1] + y[1]))\
-    #         .map(lambda x: (x[0], x[1][0], x[1][1]))\
-    #         .toDF(sample_columns)
 
     # todo: dynamic change table name
     sample_table_name = 'algorithm.tmp_aip_sample_gaoqian'
-    print(sample_df.show(20))
-    sample_df.write.format("hive").mode("overwrite").saveAsTable(sample_table_name)
+    print(sample.show(20))
+    sample.write.format("hive").mode("overwrite").saveAsTable(sample_table_name)
     return sample_table_name, sample_columns
 
 
