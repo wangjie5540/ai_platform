10d92f9e287952cc3b15ea654132a1d598fe5f44
王同贵 <wangtonggui@digitforce.com>
2023-02-28 16:14:44 +0800

    优化自定义配置的读取

--- digitforce/aip/common/utils/config_helper.py
+++ digitforce/aip/common/utils/config_helper.py
@@ -1,6 +1,7 @@
 # coding: utf-8
 
 import yaml
+import os
 import digitforce.aip.common.constants.global_constant as global_constant
 
 
@@ -11,6 +12,8 @@ def get_module_config(module_name):
 
 
 def get_component_config(module_name):
+    if not os.path.exists(global_constant.COMPONENT_CONFIG_PATH):
+        return {}
     with open(global_constant.COMPONENT_CONFIG_PATH, 'r') as f:
         config = yaml.load(f, Loader=yaml.FullLoader)
         return config.get(module_name, {})
