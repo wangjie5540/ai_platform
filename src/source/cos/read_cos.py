12e0a1128e917922d8b7bbf56454260e45f53d23
王同贵 <wangtonggui@digitforce.com>
2023-03-02 20:37:20 +0800

    调试source-cos

--- src/source/cos/read_cos.py
+++ src/source/cos/read_cos.py
@@ -1,11 +1,9 @@
 import digitforce.aip.common.utils.spark_helper as spark_helper
 import pandas as pd
 import digitforce.aip.common.utils.id_helper as id_helper
-import time
 
 
 def read_to_table(url: str, columns: str):
-    time.sleep(10 * 60)
     column_list = columns.strip().split(',')
     df = pd.read_csv(url, names=column_list)
     df = spark_helper.SparkClient.get().get_session().createDataFrame(df)
