7c597e983ac134c1165aa7e6ac332323df1512bc
zhangyifan1 <zhangyifan1@digitforce.com>
2023-03-06 11:03:00 +0000

    事件参数调整

--- src/sample/sample_selection_dixiaohu/main.py
+++ src/sample/sample_selection_dixiaohu/main.py
@@ -22,9 +22,19 @@ def run():
     dixiao_after_days = global_params[args.name]['dixiao_after_days']
     right_zc_threshold = global_params[args.name]['right_zc_threshold']
     avg_zc_threshold = global_params[args.name]['avg_zc_threshold']
-    event_tag = str(global_params[args.name]['event_tag'])
+    event_tag = global_params[args.name]['event_tag']
 
+    print(f"global_params:{global_params}")
+    print(f"name:{args.name}")
+    print(f"sample_table_name:{sample_table_name}")
+    print(f"event_tag:{event_tag}")
 
+    event_map = {
+        0: "login",
+        1: "exchange",
+    }
+    event_tag = event_map.get(event_tag)
+    
     sample_table_name = start_sample_selection(
         dixiao_before_days=dixiao_before_days,
         dixiao_after_days=dixiao_after_days,
