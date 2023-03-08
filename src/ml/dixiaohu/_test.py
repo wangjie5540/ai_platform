cd003c317182ae0cda95fcd3b58621ff304dee55
zhangyifan1 <zhangyifan1@digitforce.com>
2023-03-08 05:37:14 +0000

    删除测试notebook，修改测试文件

--- src/ml/dixiaohu/_test.py
+++ src/ml/dixiaohu/_test.py
@@ -3,39 +3,19 @@
 
 from model_train import start_model_train
 
-train_table_name = "algorithm.aip_zq_liushi_custom_feature_train"
-test_table_name = "algorithm.aip_zq_liushi_custom_feature_test"
-learning_rate = 0.05
-n_estimators = 200
-max_depth = 5
-scale_pos_weight = 0.5
-# start_model_train(train_table_name, test_table_name,
-#                   learning_rate=learning_rate, n_estimators=n_estimators, max_depth=max_depth,
-#                   scale_pos_weight=scale_pos_weight,
-#                   is_automl=False)
+train_table_name = "algorithm.aip_zq_dixiaohu_custom_feature_train_standarddata"
+test_table_name = "algorithm.aip_zq_dixiaohu_custom_feature_test_standarddata"
+model_and_metrics_data_hdfs_path = "/user/ai/aip/model/1632699470585520130"
 
-# for test
-import os
-import json
+start_model_train(
+    train_table_name,
+    test_table_name,
+    learning_rate=0.05,
+    n_estimators=200,
+    max_depth=5,
+    scale_pos_weight=0.5,
+    is_automl=False,
+    model_and_metrics_data_hdfs_path=model_and_metrics_data_hdfs_path,
+)
 
-train_table_name = "algorithm.aip_zq_liushi_custom_feature_train"
-test_table_name = "algorithm.aip_zq_liushi_custom_feature_test"
-learning_rate = 0.05
-n_estimators = 200
-max_depth = 5
-scale_pos_weight = 0.5
-os.environ["global_params"] = json.dumps(
-    {"op_name": {
-        "train_data": train_table_name,
-        "learning_rate": learning_rate,
-        "test_data": test_table_name,
-        "n_estimators": n_estimators,
-        "max_depth": max_depth,
-        "scale_pos_weight": scale_pos_weight,
-        "is_automl": False,
-        "model_and_metrics_data_hdfs_path": "/user/ai/aip/tmp"
-    }})
-os.environ["name"] = "op_name"
-from main import run
 
-run()
