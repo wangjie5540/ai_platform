8c67931993d3ed2c7362e2f97397e594a0f64531
王同贵 <wangtonggui@digitforce.com>
2023-03-30 15:09:18 +0800

    添加starrocks helper

--- pipelines/demo/self_inspection/pipeline.py
+++ pipelines/demo/self_inspection/pipeline.py
@@ -9,7 +9,7 @@ pipeline_name = 'self_inspection'
 
 @kfp.dsl.pipeline(name=pipeline_name)
 def pipeline_func(global_params):
-    print_op = DemoPrint(name='demo_print', global_params=global_params, tag='latest')
-    sleep_op = DemoSleep(name='demo_sleep', global_params=global_params, tag='latest')
+    print_op = DemoPrint(name='demo_print', global_params=global_params, tag='1.0.4')
+    sleep_op = DemoSleep(name='demo_sleep', global_params=global_params, tag='1.0.3')
     read_cos_op = Cos(name='demo_read_cos', global_params=global_params, tag='latest')
-    read_starrocks_op = DemoStarrocks(name="demo_read_starrocks", global_params=global_params, tag='latest')
+    read_starrocks_op = DemoStarrocks(name="demo_read_starrocks", global_params=global_params, tag='1.0.3')
