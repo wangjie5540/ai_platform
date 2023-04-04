4e8fe31c5239de2b09d7dda0fc60e363528404c7
王同贵 <wangtonggui@digitforce.com>
2023-04-04 15:33:39 +0800

    read cos组件添加x_tenant的header

--- src/source/cos/_test.py
+++ src/source/cos/_test.py
@@ -1,3 +1,3 @@
 import read_cos
 
-read_cos.read_to_table("http://algorithm-1308011215.cos.ap-beijing.myqcloud.com/result.csv", "a,b")
+read_cos.read_to_table("http://172.22.20.45:8658/api/labelx/open/pack/1185/download?targetEntityId=93", "user_id", x_tenant="eyJkZXBsb3lNb2RlIjoiU0hBUkUiLCJuYW1lc3BhY2UiOiJERUZBVUxUIiwidGVuYW50SWQiOjEwMDAwLCJ1c2VyQWNjb3VudCI6ImNoYW5nanllIiwidXNlck5hbWUiOiJjaGFuZ2ppZSJ9")
--- src/source/cos/main.py
+++ src/source/cos/main.py
@@ -10,9 +10,10 @@ def run():
     parser.add_argument('--name', type=str, required=True, help='名称')
     parser.add_argument('--global_params', type=str, required=True, help='pipeline全局参数')
     args = parser.parse_args()
+    x_tenant = json.loads(args.global_params).get('X_TENANT', None)
     url = json.loads(args.global_params)[args.name]['url']
     columns = json.loads(args.global_params)[args.name]['columns']
-    table_name, columns = read_cos.read_to_table(url, columns)
+    table_name, columns = read_cos.read_to_table(url, columns, x_tenant)
     outputs = {
         "type": "hive_table",
         "table_name": table_name,
