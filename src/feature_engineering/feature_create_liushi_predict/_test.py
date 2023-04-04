a9f92abde27475ef25c20f14eac19b9f8ee15c74
wh <wanghan@digitforce.com>
2023-04-04 18:50:55 +0800

    更新主分支代码

--- src/feature_engineering/feature_create_liushi_predict/_test.py
+++ src/feature_engineering/feature_create_liushi_predict/_test.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # encoding: utf-8
-from feature_create import feature_create
-
-predict_table_name = "aip.cos_4728115722869280769"
-active_before_days = 1
-active_after_days = 1
-
-predict_feature_table_name = feature_create(predict_table_name,
-                                            active_before_days, active_after_days,
-                                            feature_days=30)
+# from feature_create import feature_create
+#
+# predict_table_name = "aip.cos_4736824944830189569"
+# active_before_days = 1
+# active_after_days = 1
+#
+# predict_feature_table_name = feature_create(predict_table_name,
+#                                             active_before_days, active_after_days,
+#                                             feature_days=30)
