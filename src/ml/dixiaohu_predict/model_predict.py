f7ac6121c63655feedc0236075af3c3ca523834b
Wings <zhangyifan1@digitforce.com>
2023-04-07 17:09:13 +0800

    添加传参，添加解释统计部分

--- src/ml/dixiaohu_predict/model_predict.py
+++ src/ml/dixiaohu_predict/model_predict.py
@@ -11,16 +11,21 @@ from digitforce.aip.common.utils.spark_helper import SparkClient
 
 hdfs_client = hdfs_helper.HdfsClient()
 
+from digitforce.aip.common.utils.starrocks_helper import write_score
+
 
 def start_model_predict(
-        predict_table_name: str, model_hdfs_path: str, output_file_name: str
+        predict_feature_table_name: str, model_hdfs_path: str, output_file_name: str,
+        instance_id: int, predict_table_name: str
 ):
     """
     预测过程
     Args:
-        predict_table_name:
-        model_hdfs_path:
-        output_file_name:
+        instance_id: 预测任务id，需要接收的
+        predict_table_name: 预测分数存储表名
+        predict_feature_table_name: 预测特征表名
+        model_hdfs_path: 模型存储hdfs地址
+        output_file_name: 输出文件名称，需要输出的
     """
     spark_client = SparkClient.get()
     spark = spark_client.get_session()
@@ -28,7 +33,7 @@ def start_model_predict(
 
     df_predict = spark.sql(
         f"""
-        select * from {predict_table_name}
+        select * from {predict_feature_table_name}
         """
     ).toPandas()
     # 连续特征，离散特征，丢弃特征等的处理
@@ -89,6 +94,34 @@ def start_model_predict(
     result.to_csv(result_local_path, index=False, header=False)
     output_file_path = cos_helper.upload_file("result.csv", output_file_name)
     print("output_file_path-----*****", output_file_path)
+
+    # 统计和可解释性部分
+    result["instance_id"] = instance_id
+    result = result.rename(columns={"cust_code": "user_id"})
+    print("result", result)
+    result_spark_df = spark.createDataFrame(result)
+    # 存储到starrocks,列名[['instance_id','user_id','score']]
+    write_score(result_spark_df, predict_table_name)
+
+    # from digitforce.aip.common.utils.explain_helper import explain_main
+    # # shap和spark存在兼容性冲突，须放在spark_client后使用
+    # # 计算ale值和shap值并存储
+    # ale_df, shap_df = explain_main(
+    #     df_predict.drop(columns=["label", "dt"]), model, categorical_features
+    # )
+    # # 存储ale
+    # ale_local_path = "ale.csv"
+    # ale_hdfs_path = "/user/ai/aip/zq/dixiaohu/explain/_ale.csv"
+    # ale_df.to_csv(ale_local_path, index=False, header=False)
+    # write_hdfs_path(ale_local_path, ale_hdfs_path, hdfs_client)
+    # print("ale_ 计算存储完成-----*****", ale_hdfs_path)
+    #
+    # # 存储shap到starrocks
+    # shap_local_path = "shap.csv"
+    # shap_hdfs_path = "/user/ai/aip/zq/dixiaohu/explain/_shap.csv"
+    # shap_df.to_csv(shap_local_path, index=False, header=False)
+    # write_hdfs_path(shap_local_path, shap_hdfs_path, hdfs_client)
+    # print("shap 计算存储完成-----*****", shap_hdfs_path)
 
 
 # 读hdfs
