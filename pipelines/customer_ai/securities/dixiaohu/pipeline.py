763a35fb9becb44e36e186e1244e6ddb1a37cdd5
Wings <zhangyifan1@digitforce.com>
2023-04-07 11:39:59 +0800

    2.0.0不添加可解释和roc

--- pipelines/customer_ai/securities/dixiaohu/pipeline.py
+++ pipelines/customer_ai/securities/dixiaohu/pipeline.py
@@ -1,5 +1,4 @@
 
-
 from digitforce.aip.components.feature_engineering import (
     FeatureCreateDixiaohu,
     FeatureCreateDixiaohuPredict,
@@ -10,19 +9,20 @@ from digitforce.aip.components.source.cos import Cos
 from kfp.dsl import Condition
 import kfp.dsl as dsl
 
-pipeline_name = "dixiaohu_test"
+pipeline_name = "dixiaohu_prod"
 
 @dsl.pipeline(name=pipeline_name)
 def pipeline_func(global_params: str, flag="TRAIN"):
+    RUN_ENV = "2.0.0"
     with Condition(flag != "PREDICT", name="is_not_predict"):
         op_sample_selection = SampleSelectionDixiaohu(
-            name="sample_select", global_params=global_params, tag="latest"
+            name="sample_select", global_params=global_params, tag=RUN_ENV
         )
 
         op_feature_create = FeatureCreateDixiaohu(
             name="feature_create",
             global_params=global_params,
-            tag="latest",
+            tag=RUN_ENV,
             sample=op_sample_selection.outputs["sample_table_name"],
         )
 
@@ -30,7 +30,7 @@ def pipeline_func(global_params: str, flag="TRAIN"):
             op_sample_comb = DixiaohuModel(
                 name="model",
                 global_params=global_params,
-                tag="latest",
+                tag=RUN_ENV,
                 train_table_name=op_feature_create.outputs[
                     op_feature_create.OUTPUT_TRAIN_FEATURE
                 ],
@@ -45,16 +45,13 @@ def pipeline_func(global_params: str, flag="TRAIN"):
             name="feature_create_predict",
             global_params=global_params,
             sample=predict_table_op.outputs[Cos.OUTPUT_1],
-            tag="latest",
+            tag=RUN_ENV,
         )
         dixiaohu_predict_op = DixiaohuPredict(
             name="model_predict",
             global_params=global_params,
-            tag="latest",
+            tag=RUN_ENV,
             predict_table_name=predict_feature_op.outputs[
                 predict_feature_op.OUTPUT_PREDICT_FEATURE
             ],
         )
-
-
-
