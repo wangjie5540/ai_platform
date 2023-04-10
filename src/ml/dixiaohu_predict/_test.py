b5aaec7fd98a70ac860a9a1c2d0d560aed0099d7
Wings <zhangyifan1@digitforce.com>
2023-04-10 11:43:17 +0800

    修改测试文件

--- src/ml/dixiaohu_predict/_test.py
+++ src/ml/dixiaohu_predict/_test.py
@@ -5,8 +5,8 @@ from model_predict import start_model_predict
 predict_feature_table_name = "algorithm.aip_zq_dixiaohu_custom_feature_predict_standarddata"
 model_hdfs_path = "/user/ai/aip/model/222/model.pk"
 output_file_name = "result.csv"
-instance_id = 111
-predict_table_name = "aip.score_241"
+instance_id = 3
+predict_table_name = "score_241"
 start_model_predict(predict_feature_table_name=predict_feature_table_name
                     , model_hdfs_path=model_hdfs_path
                     , output_file_name=output_file_name
