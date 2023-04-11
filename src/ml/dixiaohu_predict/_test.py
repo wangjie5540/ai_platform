98af9cb808c96530de33f43618b334e3e1af4f5d
Wings <zhangyifan1@digitforce.com>
2023-04-11 11:12:03 +0800

    调整测试参数

--- src/ml/dixiaohu_predict/_test.py
+++ src/ml/dixiaohu_predict/_test.py
@@ -3,7 +3,10 @@
 from model_predict import start_model_predict
 
 predict_feature_table_name = "algorithm.aip_zq_dixiaohu_custom_feature_predict_standarddata"
-model_hdfs_path = "/user/ai/aip/zq/dixiaohu/model/latest_model.pk"
+# model_hdfs_path = "/user/ai/aip/zq/dixiaohu/model/latest_model.pk"
+# model_hdfs_path = "/user/ai/aip/zq/dixiaohu/model/2023-04-10.model"
+model_hdfs_path = "/user/ai/aip/zq/dixiaohu/model/latest_model.pickle.dat"
+# print("model_hdfs_path:", model_hdfs_path)
 output_file_name = "result.csv"
 instance_id = 3
 predict_table_name = "score_249"
