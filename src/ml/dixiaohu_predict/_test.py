a35848ea09728249f7ef4f151371c784831beb41
Wings <zhangyifan1@digitforce.com>
2023-04-14 20:11:20 +0800

    调整测试文件

--- src/ml/dixiaohu_predict/_test.py
+++ src/ml/dixiaohu_predict/_test.py
@@ -8,11 +8,12 @@ predict_feature_table_name = "algorithm.aip_zq_dixiaohu_custom_feature_predict_s
 model_hdfs_path = "/user/ai/aip/zq/dixiaohu/model/latest_model.pickle.dat"
 # print("model_hdfs_path:", model_hdfs_path)
 output_file_name = "result.csv"
-instance_id = 3
-predict_table_name = "score_249"
+instance_id = 1646809101662289921
+predict_table_name = "aip.score_254"
+shapley_table_name = "aip.shapley_254"
 start_model_predict(predict_feature_table_name=predict_feature_table_name
                     , model_hdfs_path=model_hdfs_path
                     , output_file_name=output_file_name
                     , instance_id=instance_id
-                    , predict_table_name=predict_table_name)
-
+                    , predict_table_name=predict_table_name
+                    , shapley_table_name=shapley_table_name)
