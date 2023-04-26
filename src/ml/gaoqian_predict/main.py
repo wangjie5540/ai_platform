966e9fdaa998683a28b1b7400873ac885e6a0333
汤思璇 <tangsixuan@digitforce.com>
2023-04-26 11:14:08 +0800

    更新参数传递

--- src/ml/gaoqian_predict/main.py
+++ src/ml/gaoqian_predict/main.py
@@ -6,28 +6,37 @@ import os
 from digitforce.aip.common.utils import component_helper
 component_helper.init_config()
 from model_predict import start_model_predict
-from digitforce.aip.common.utils.argument_helper import df_argument_helper
+# from digitforce.aip.common.utils.argument_helper import df_argument_helper
 
 def run():
     # 参数解析
-    df_argument_helper.add_argument("--global_params", type=str, required=True, help="全局参数")
-    df_argument_helper.add_argument("--name", type=str, required=True, help="名称")
-    df_argument_helper.add_argument("--predict_table_name", type=str, required=False, help="预测数据")
-    df_argument_helper.add_argument("--model_hdfs_path", type=str, required=False, help="模型地址")
-    df_argument_helper.add_argument("--output_file_name", type=str, required=False, help="输出文件名称")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--global_params", type=str, required=True, help="全局参数")
+    parser.add_argument("--name", type=str, required=True, help="名称")
+    parser.add_argument("--predict_table_name", type=str, required=True,
+                        help="预测样本数据")
+    args = parser.parse_args()
 
-    predict_table_name = df_argument_helper.get_argument("predict_table_name")
-    model_hdfs_path = df_argument_helper.get_argument("model_hdfs_path")
-    output_file_name = df_argument_helper.get_argument("output_file_name")
+    global_params = args.global_params
+    predict_feature_table_name = args.predict_table_name
+    global_params = json.loads(global_params)
 
-    instance_id = df_argument_helper.get_argument("instance_id")
-    predict_score_table_name = df_argument_helper.get_argument("predict_table_name")
-    shapley_table_name = df_argument_helper.get_argument("shapley_table_name")
+    # global_params下model_predict组件的两个变量
+    model_hdfs_path = global_params[args.name]['model_hdfs_path']
+    output_file_name = global_params[args.name]['output_file_name']
+    # instance_id和存储表名由上游给出
+    instance_id = global_params['instance_id']
+    predict_score_table_name = global_params['predict_table_name']
+    shapley_table_name = global_params['shapley_table_name']
 
-    print(f"predict_table_name:{predict_table_name}")
+    print(f"predict_table_name:{predict_feature_table_name}")
     print(f"model_hdfs_path:{model_hdfs_path}")
     print(f"output_file_name:{output_file_name}")
-    start_model_predict(predict_table_name, model_hdfs_path, output_file_name, instance_id, predict_score_table_name, shapley_table_name)
+
+    print(f"predict_table_name:{predict_feature_table_name}")
+    print(f"model_hdfs_path:{model_hdfs_path}")
+    print(f"output_file_name:{output_file_name}")
+    start_model_predict(predict_feature_table_name, model_hdfs_path, output_file_name, instance_id, predict_score_table_name, shapley_table_name)
 
 
 
