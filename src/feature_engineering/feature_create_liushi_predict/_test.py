fb15e00ce212818871750561a36c869c49c85bca
wh <wanghan@digitforce.com>
2023-04-26 10:17:42 +0800

    debug

--- src/feature_engineering/feature_create_liushi_predict/_test.py
+++ src/feature_engineering/feature_create_liushi_predict/_test.py
@@ -2,8 +2,8 @@
 # encoding: utf-8
 from feature_create import feature_create
 
-predict_table_name = "aip.cos_4740925280993939457"
-active_before_days = 1
+predict_table_name = "algorithm.aip_zq_liushi_custom_feature_predict"
+active_before_days = 3
 active_after_days = 1
 
 predict_feature_table_name = feature_create(predict_table_name,
